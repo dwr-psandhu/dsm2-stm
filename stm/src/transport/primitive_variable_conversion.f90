2ccb248913d421aca8c6f3f142bc3cc6694404b1
kuckao <kuckao@93266118-e052-11de-8dcd-9b777bee35ae>
2009-12-03 23:44:27 +0000

    First STM check-in. Diffusion module is not yet included.

---
+++ stm/src/transport/primitive_variable_conversion.f90
@@ -0,0 +1,65 @@
+!<license>
+!    Copyright (C) 1996, 1997, 1998, 2001, 2007, 2009 State of California,
+!    Department of Water Resources.
+!    This file is part of DSM2.
+!
+!    The Delta Simulation Model 2 (DSM2) is free software: 
+!    you can redistribute it and/or modify
+!    it under the terms of the GNU General Public License as published by
+!    the Free Software Foundation, either version 3 of the License, or
+!    (at your option) any later version.
+!
+!    DSM2 is distributed in the hope that it will be useful,
+!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!    GNU General Public License for more details.
+!
+!    You should have received a copy of the GNU General Public License
+!    along with DSM2.  If not, see <http://www.gnu.org/licenses>.
+!</license>
+
+module primitive_variable_conversion
+
+contains
+!> Convert conservative variables (ie mass) to primitive (concentration)
+subroutine cons2prim(conc,mass,area,nloc,nvar)
+use stm_precision
+implicit none
+real(STM_REAL),intent(out) :: conc(nloc,nvar)   !< concentration (converted from mass per unit length )
+real(STM_REAL),intent(in) :: mass(nloc,nvar)    !< mass per unit length 
+real(STM_REAL),intent(in) :: area(nloc)         !< area at conversion locations
+!--- args
+integer,intent(in)  :: nloc  !< Number of cells or faces
+integer,intent(in)  :: nvar   !< Number of variables
+!--- locals
+integer :: ivar
+!-------------------
+do ivar = 1,nvar
+    conc(:,ivar) = mass(:,ivar)/area
+end do
+return
+end subroutine
+
+!> Convert  primitive (concentration) to conservative variables (ie mass)
+subroutine prim2cons(mass,conc,area,nloc,nvar)
+use stm_precision
+implicit none
+real(STM_REAL),intent(out) :: mass(nloc,nvar) !< mass per unit length (converted from concentration)
+real(STM_REAL),intent(in) :: conc(nloc,nvar)  !< concentrations to convert
+real(STM_REAL),intent(in) :: area(nloc)       !< area at conversion locations
+!--- args
+integer,intent(in)  :: nloc  !< Number of cells or faces
+integer,intent(in)  :: nvar   !< Number of variables
+!--- locals
+integer :: ivar
+!-------------------
+
+do ivar = 1,nvar
+    mass(:,ivar) = conc(:,ivar)*area
+end do
+return
+end subroutine
+
+end module
+
+
