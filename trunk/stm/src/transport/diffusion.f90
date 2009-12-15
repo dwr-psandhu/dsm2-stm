4ea1dfeb8e229121d990784667e94f2523b0328a
kavehzamani <kavehzamani@93266118-e052-11de-8dcd-9b777bee35ae>
2009-12-15 20:04:49 +0000

    In ADV_DIF_SOURCE_2.doc correction of theta , and drafted diffusion subroutine

--- trunk/stm/src/transport/diffusion.f90
+++ trunk/stm/src/transport/diffusion.f90
@@ -23,6 +23,7 @@
 !> Explicit and implicit diffusion operators are included here.
 !>@ingroup transport
 module diffusion
+
 use stm_precision
 
 contains
@@ -50,12 +51,16 @@ subroutine diffuse(conc,     &
                   dx)
 
 use primitive_variable_conversion
+
 implicit none
+
+! ---- args
 
 integer, intent (in) :: ncell !< Number of cells
 integer, intent (in) :: nvar  !< Number of variables
 
 real(stm_real), intent (out) :: conc(ncell,nvar)     !< Concentration at new time
+real(stm_real) :: explct_diffusion_term(ncell,nvar)     !< Explicit diffusion predictor
 real(stm_real), intent (in) :: conc_prev(ncell,nvar) !< Concentration at old time
 real(stm_real), intent (in) :: area (ncell,nvar)     !< Cell-centered area at new time
 real(stm_real), intent (in) :: area_prev (ncell,nvar)!< Cell-centered area at old time
@@ -75,8 +80,29 @@ real(stm_real), intent (in) :: dx                    !< Spacial step
 ! for a single time step (ie, explicit). This is needed for the advection step.
 ! It is also probably part of the right hand side of the implicit diffusion solver 
 ! matrix calculation. 
-call explicit_diffusion_operator(,     &
-                  dx)
+
+
+
+call explicit_diffusion_operator (explct_diffusion_term, &
+!                                         conc,             &
+                                          conc_prev,        &
+                                          area,             &
+                                          area_prev,        &
+                                          area_lo,          &
+                                          area_hi,          &
+                                          ks_lo,            &
+                                          ks_hi,            &
+                                          ncell,            &
+                                          nvar,             &
+                                          time,             &
+!                                          strt_flx_prev,    &   
+!                                          end_flx_prev,     &
+!                                          strt_conc_prev,   &
+!                                          end_conc_prev,    &
+!                                          bc_flg_strt,      &
+!                                          bc_flg_end,       &
+                                          dt,               &
+                                          dx)
 
 ! 
 call construct_diffusion_matrix()
@@ -87,6 +113,103 @@ call apply_diffusion_boundary()
 
 call solve
 
-end subroutine
+
+return
+end subroutine diffuse
 
 end module diffusion
+
+!////////////////////////////
+
+subroutine  explicit_diffusion_operator (explct_diffusion_term, &
+!                                         conc,             &
+                                          conc_prev,        &
+!                                          area,             &
+                                          area_prev,        &
+                                          area_lo,          &
+                                          area_hi,          &
+                                          ks_lo,            &
+                                          ks_hi,            &
+                                          ncell,            &
+                                          nvar,             &
+                                          time,             &
+!                                          strt_flx_prev,    &   
+!                                          end_flx_prev,     &
+!                                          strt_conc_prev,   &
+!                                          end_conc_prev,    &
+!                                          bc_flg_strt,      &
+!                                          bc_flg_end,       &
+                                          dt,               &
+                                          dx)
+                                  
+use stm_precision
+
+implicit none 
+
+!----args
+
+integer, intent (in) :: ncell !< Number of cells
+integer, intent (in) :: nvar  !< Number of variables
+
+real(stm_real), intent (out) :: explct_diffusion_term(ncell,nvar)     !< Explicit diffusion predictor
+!real(stm_real), intent (out) :: conc(ncell,nvar)     !< Concentration at new time
+real(stm_real), intent (in) :: conc_prev(ncell,nvar) !< Concentration at old time
+!real(stm_real), intent (in) :: area (ncell,nvar)     !< Cell-centered area at new time
+real(stm_real), intent (in) :: area_prev (ncell,nvar)!< Cell-centered area at old time
+real(stm_real), intent (in) :: area_lo (ncell,nvar)  !< Low side area at old time
+real(stm_real), intent (in) :: area_hi (ncell,nvar)  !< High side area  
+real(stm_real), intent (in) :: ks_lo (ncell,nvar)    !< Low side sediment dispersion coef. at old time
+real(stm_real), intent (in) :: ks_hi (ncell,nvar)    !< High side sediment dispersion coef. at old time
+real(stm_real), intent (in) :: time                  !< Current time
+!
+!
+!
+!
+!
+!real(stm_real), intent (in) :: strt_flx_prev         !< flux    
+!real(stm_real), intent (in) :: theta_stm             !< Expilicitness coefficient 0 is explicit, 0.5 Crank-Nicolson, 1 full implicit  
+real(stm_real), intent (in) :: dt                    !< Time step   
+real(stm_real), intent (in) :: dx                    !< Spacial step  
+
+!--- locals
+integer :: ivar
+integer :: icell
+
+real(stm_real) :: dtbydx2
+
+
+dtbydx2 = dt/dx/dx
+
+do ivar = 1, nvar 
+    do icell = 2,ncell-1
+    
+        if ( ks_lo(icell,ivar)*dtbydx2> half .or. ks_hi(icell,ivar)*dtbydx2 > half) then 
+        print *, 'Unsatabality in solution!'
+        pause
+        end if 
+        
+!        conc(icell,ivar) = area_prev(icell,ivar)* conc_prev(icell,ivar)
+!        conc(icell,ivar) = conc(icell,ivar) + dtbydx2 * area_hi(icell,ivar) * ks_hi(icell,ivar)*conc_prev(icell+1,ivar)
+!        conc(icell,ivar) = minus * dtbydx2 * area_hi(icell,ivar) * ks_hi(icell,ivar)
+!       
+!        
+!        conc(icell,ivar) = conc(icell,ivar) /area (icell,ivar)
+        
+        
+        
+        
+    
+    end do
+
+end do
+
+
+
+
+                                  
+                                  
+                                  
+return
+end subroutine explicit_diffusion_operator 
+                                 
+
