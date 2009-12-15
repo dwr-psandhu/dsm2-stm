1d0205b29c95c78cd19dd31e6e15799144507a80
kavehzamani <kavehzamani@93266118-e052-11de-8dcd-9b777bee35ae>
2009-12-15 09:26:09 +0000

    Basic diffusion.f90

--- trunk/stm/src/transport/diffusion.f90
+++ trunk/stm/src/transport/diffusion.f90
@@ -27,11 +27,13 @@ use stm_precision
 
 contains
 
-! This subroutine calculates the diffusive portion of the constituent transport.
-! It contains an explicit version of the diffusion operator and a general (involving all
-! potential cases) diffusion operator as well, with a coefficient theta_stm for 
-! selecting the level of implicitness. (theta_stm=0.5 is Crank Nicolson.).
-! The matrix is solved via a tri-diagonal solver.  
+! ///////////////////////////////////////////////////////////////////
+
+!> Calculates the diffusive portion of the constituent transport.
+!> It contains an explicit version of the diffusion operator and a general (involving all
+!> potential cases) diffusion operator as well, with a coefficient theta_stm for 
+!> selecting the level of implicitness. (theta_stm=0.5 is Crank Nicolson.).
+!> The matrix is solved via a tri-diagonal solver.  
 subroutine diffuse(conc,     &
                   conc_prev,&
                   area,     &
@@ -50,11 +52,30 @@ subroutine diffuse(conc,     &
 use primitive_variable_conversion
 implicit none
 
+integer, intent (in) :: ncell !< Number of cells
+integer, intent (in) :: nvar  !< Number of variables
+
+real(stm_real), intent (out) :: conc(ncell,nvar)     !< Concentration at new time
+real(stm_real), intent (in) :: conc_prev(ncell,nvar) !< Concentration at old time
+real(stm_real), intent (in) :: area (ncell,nvar)     !< Cell-centered area at new time
+real(stm_real), intent (in) :: area_prev (ncell,nvar)!< Cell-centered area at old time
+real(stm_real), intent (in) :: area_lo (ncell,nvar)  !< Low side area centered intime
+real(stm_real), intent (in) :: area_hi (ncell,nvar)  !< High side area centered in time 
+real(stm_real), intent (in) :: ks_lo (ncell,nvar)    !< Low side sediment dispersion coef. centered in time
+real(stm_real), intent (in) :: ks_hi (ncell,nvar)    !< High side sediment dispersion coef. centered in time
+real(stm_real), intent (in) :: time                  !< Current time
+real(stm_real), intent (in) :: theta_stm             !< Expilicitness coefficient 0 is explicit, 0.5 Crank-Nicolson, 1 full implicit  
+real(stm_real), intent (in) :: dt                    !< Time step   
+real(stm_real), intent (in) :: dx                    !< Spacial step  
+
+
+
+
 ! This routine gives the effects of diffusion fluxes on each cell
 ! for a single time step (ie, explicit). This is needed for the advection step.
 ! It is also probably part of the right hand side of the implicit diffusion solver 
 ! matrix calculation. 
-call explicit_diffusion_operator(mass,     &
+call explicit_diffusion_operator(,     &
                   dx)
 
 ! 
