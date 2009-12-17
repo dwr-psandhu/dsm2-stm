d840004d0411273335fcae52010c32a80443e944
kavehzamani <kavehzamani@93266118-e052-11de-8dcd-9b777bee35ae>
2009-12-17 19:57:32 +0000

    change in diffusion.f90 variables and documenting

--- stm/src/transport/diffusion.f90
+++ stm/src/transport/diffusion.f90
@@ -35,164 +35,142 @@ contains
 !> It contains an explicit version of the diffusion operator and a general (involving all
 !> potential cases) diffusion operator as well, with a coefficient theta_stm for 
 !> selecting the level of implicitness. (theta_stm=0.5 is Crank Nicolson.).
-!> The matrix is solved via a tri-diagonal solver.  !todo: missing boundary value for Diritchlet BC?
-subroutine diffuse(conc,     &
-                  conc_prev,&
-                  mass,     &
-                  mass_prev, &
-                  area,     &
-                  area_prev,&
-                  area_lo,  &
-                  area_hi,  &
-                  ks_lo,    &  !todo: rename diff_coeff_lo 
-                  ks_hi,    &
-                  ncell,    &
-                  nvar,     &
-                  time,     &
-                  theta_stm,&
-                  dt,       &
-                  dx,       &
-                  bc_end_flux, &
-                  bc_start_flux  )
+!> The matrix is solved via a tri-diagonal solver.  !
+!> The algoritm looks like this:
+!>   - This creates the diffusive fluxes sends them for modification for boundaries and then differences the fluxes to get the operator d/dx(Ad/dx). 
+!>         - Calculate interior and boundary fluxes 
+!>   - Construct right hand side with Nuemann boundary condition imposed
+!>   - Construct diffusion coefficeint matrix with Nuemann boundary condition imposed
+!>   - Solve the system
+subroutine diffuse(conc,             &
+                  conc_prev,         &
+                  mass,              &
+                  mass_prev,         &
+                  area,              &
+                  area_prev,         &
+                  area_lo,           &
+                  area_hi,           &
+                  disp_coef_lo,      &  
+                  disp_coef_hi,      &
+                  disp_coef_lo_prev, &  
+                  disp_coef_hi_prev, &
+                  ncell,             &
+                  nvar,              &
+                  time,              &
+                  theta_stm,         &
+                  dt,                &
+                  dx                 )
 
-use primitive_variable_conversion ! does it needed here 
+use primitive_variable_conversion 
 
 implicit none
 
 ! ---- args
 
-integer, intent (in) :: ncell !< Number of cells
-integer, intent (in) :: nvar  !< Number of variables
+integer, intent (in) :: ncell                                !< Number of cells
+integer, intent (in) :: nvar                                 !< Number of variables
 
-real(stm_real), intent (out) :: conc(ncell,nvar)     !< Concentration at new time
-real(stm_real), intent (out) :: explicit_diffusion_term(ncell,nvar)     !< Explicit diffusion predictor
-real(stm_real), intent (out) :: mass(ncell,nvar)     !< mass (A*C) at new time
-real(stm_real), intent (in) :: mass_prev(ncell,nvar) !< mass (A*C) at old time
-real(stm_real), intent (in) :: conc_prev(ncell,nvar) !< Concentration at old time
-real(stm_real), intent (in) :: area (ncell,nvar)     !< Cell-centered area at new time
-real(stm_real), intent (in) :: area_prev (ncell,nvar)!< Cell-centered area at old time
-real(stm_real), intent (in) :: area_lo (ncell,nvar)  !< Low side area centered intime
-real(stm_real), intent (in) :: area_hi (ncell,nvar)  !< High side area centered in time 
-real(stm_real), intent (in) :: ks_lo (ncell,nvar)    !< Low side sediment dispersion coef. centered in time
-real(stm_real), intent (in) :: ks_hi (ncell,nvar)    !< High side sediment dispersion coef. centered in time
-real(stm_real), intent (in) :: time                  !< Current time
-real(stm_real), intent (in) :: theta_stm             !< Expilicitness coefficient 0 is explicit, 0.5 Crank-Nicolson, 1 full implicit  
-real(stm_real), intent (in) :: dt                    !< Time step   
-real(stm_real), intent (in) :: dx                    !< Spacial step 
-real(stm_real), intent (in) :: bc_end_flux         !< flux at right hand side old time
-real(stm_real), intent (in) :: bc_start_flux         !< flux at left hand side old time
+real(stm_real), intent (out) :: conc(ncell,nvar)             !< Concentration at new time
+real(stm_real), intent (out) :: mass(ncell,nvar)             !< Mass (A*C) at new time
+real(stm_real), intent (in) :: mass_prev(ncell,nvar)         !< Mass (A*C) at old time
+real(stm_real), intent (in) :: conc_prev(ncell,nvar)         !< Concentration at old time
+real(stm_real), intent (in) :: area (ncell,nvar)             !< Cell-centered area at new time
+real(stm_real), intent (in) :: area_prev (ncell,nvar)        !< Cell-centered area at old time
+real(stm_real), intent (in) :: area_lo (ncell,nvar)          !< Low side area centered in time
+real(stm_real), intent (in) :: area_hi (ncell,nvar)          !< High side area centered in time 
+real(stm_real), intent (in) :: disp_coef_lo (ncell,nvar)     !< Low side constituent dispersion coef. at new time
+real(stm_real), intent (in) :: disp_coef_hi (ncell,nvar)     !< High side constituent dispersion coef. at new time
+real(stm_real), intent (in) :: disp_coef_lo_prev(ncell,nvar) !< Low side constituent dispersion coef. at old time
+real(stm_real), intent (in) :: disp_coef_hi_prev(ncell,nvar) !< High side constituent dispersion coef. at old time
+real(stm_real), intent (in) :: time                          !< Current time
+real(stm_real), intent (in) :: theta_stm                     !< Explicitness coefficient 0 is explicit, 0.5 Crank-Nicolson, 1 full implicit  
+real(stm_real), intent (in) :: dt                            !< Time step   
+real(stm_real), intent (in) :: dx                            !< Spacial step 
 
 ! ---- locals
-
-
-
-!-----
-
-! TODO: remove this
- conc = 0
- mass = 0
-
 
 ! This routine gives the effects of diffusion fluxes on each cell
 ! for a single time step (ie, explicit). This is needed for the advection step.
 ! It is also probably part of the right hand side of the implicit diffusion solver 
 ! matrix calculation. 
 
-
-
 ! Explicit diffusion operator construction. This creates the diffusive fluxes
 ! sends them for modification for boundaries and then differences the fluxes
 ! to get the operator d/dx(Ad/dx). 
 call explicit_diffusion_operator(explicit_diffuse_op, &
                                         conc_prev,        &
-                                        area,             &
-                                        area_lo,          &
-                                        area_hi,          &
-                                        disp_coef_lo,     &  
-                                        disp_coef_hi,     &
+                                        area_lo_prev,     &
+                                        area_hi_prev,     &
+                                        disp_coef_lo_prev,&  
+                                        disp_coef_hi_prev,&
                                         ncell,            &
                                         nvar,             &
                                         time,             &
                                         dx)
 
+                                        
+                                       
+
 ! todo: Define what exactly this does. It should contain mass at the old time step
 ! plus the diffusion operator at the old time step including boundary adjustment.
-call construct_right_hand_side( right_hand_side,         & ! todo : what is the name? rhs? 
-                                  explicit_diffuse_op,   & ! todo: should we use it?
-                                  area_prev,         &
-                                  area_lo_prev,      &
-                                  area_hi_prev,      &
-                                  disp_coef_lo_prev, &
-                                  disp_coef_hi_prev, &
-                                  conc_prev,         &
-                                  theta_stm,         &
-                                  ncell,             &
-                                  time,              &  ! todo: do we need time?
-                                  dx,                &
+call construct_right_hand_side( right_hand_side,         & 
+                                  explicit_diffuse_op,   & 
+                                  area_prev,             &
+                                  area_lo_prev,          &
+                                  area_hi_prev,          &
+                                  disp_coef_lo_prev,     &
+                                  disp_coef_hi_prev,     &
+                                  conc_prev,             &
+                                  theta_stm,             &
+                                  ncell,                 &
+                                  time,                  &  
+                                  dx,                    &
                                   dt)
                                         
 ! Construct the matrix for the diffusion solver
 ! without boundary condition modification or structure on interior of domain
-call construct_diffusion_matrix( center_diag ,     &
-                                  up_diag,       &     
-                                  down_diag,      &
-                                  area,         &
-                                  area_lo,       &
-                                  area_hi,       &
-                                  disp_coef_lo, &
-                                  disp_coef_hi, &
-                                  theta_stm,    &
-                                  ncell,        &
-                                  time,         &  ! todo: do we need time?
-                                  dx,           &
+call construct_diffusion_matrix( center_diag ,      &
+                                  up_diag,          &     
+                                  down_diag,        &
+                                  area,             &
+                                  area_lo,          &
+                                  area_hi,          &
+                                  disp_coef_lo,     &
+                                  disp_coef_hi,     &
+                                  theta_stm,        &
+                                  ncell,            &
+                                  time,             &  
+                                  dx,               &
                                   dt)
 
 ! this function will add boundary conditions to the matrix
-! todo
-call apply_diffusion_boundary(center_diag ,         &
-                                  up_diag,          &     
-                                  down_diag,        &
-                                  right_hand_side,     &
-                                  end_bc_type_flag,         & ! todo: is there any way to distinguish Drichlet from Nuemann base on the input type?
-                                  start_bc_type_flag,       &
-                                  bc_start_value,   &
-                                  bc_start_flux,    &
-                                  bc_end_value,     &
-                                  bc_end_flux,      &
-                                  bc_start_value_prev,   &
-                                  bc_start_flux_prev,    &
-                                  bc_end_value_prev,     &
-                                  bc_end_flux_prev,      
-                                  time             )
 
 
-
-
-
-    call solve ( center_diag ,         &
-                              up_diag,          &     
-                              down_diag,        &
-                              right_hand_side,     &
-                              conc)
+    call solve ( center_diag ,      &
+                  up_diag,          &     
+                  down_diag,        &
+                  right_hand_side,  &
+                  conc)
 
 
 return
 end subroutine diffuse
 
-!todo: totally fine to create an explicit update, but not the main thing we need right now.
 
-!> Calculate the explicit diffusion term.
+!> Calculate the explicit diffusion operator.
+!> Explicit diffusion operator construction. This creates the diffusive fluxes
+!> sends them for modification for boundaries and then differences the fluxes
+!> to get the operator d/dx(Ad/dx). 
 subroutine explicit_diffusion_operator (explicit_diffuse_op, &
-                                           conc_prev,        &
-                                           area,             &
-                                           area_lo,          &
-                                           area_hi,          &
-                                           disp_coef_lo,     &  
-                                           disp_coef_hi,     &
-                                           ncell,            &
-                                           nvar,             &
-                                           time,             &
-                                           dx)
+                                        conc_prev,        &
+                                        area_lo_prev,     &
+                                        area_hi_prev,     &
+                                        disp_coef_lo_prev,&  
+                                        disp_coef_hi_prev,&
+                                        ncell,            &
+                                        nvar,             &
+                                        time,             &
+                                        dx)
                                                                                           
 use stm_precision
 
@@ -203,15 +181,14 @@ implicit none
 integer, intent (in) :: ncell !< Number of cells
 integer, intent (in) :: nvar  !< Number of variables
 
-real(stm_real), intent (out) :: explicit_diffusion_term(ncell,nvar)     !< Explicit diffusion predictor
-real(stm_real), intent (in)  :: conc_prev(ncell,nvar)  !< Concentration at old time
-real(stm_real), intent (in)  :: area (ncell,nvar)      !< Cell-centered area at new time
-real(stm_real), intent (in)  :: area_lo (ncell,nvar)   !< Low side area at old time
-real(stm_real), intent (in)  :: area_hi (ncell,nvar)   !< High side area  
-real(stm_real), intent (in)  :: disp_coef_lo (ncell,nvar)     !< Low side sediment dispersion coef. at old time
-real(stm_real), intent (in)  :: disp_coef_hi (ncell,nvar)     !< High side sediment dispersion coef. at old time
-real(stm_real), intent (in)  :: time                   !< Current time
-real(stm_real), intent (in)  :: dx                     !< Spacial step  
+real(stm_real), intent (out) :: explicit_diffuse_op(ncell,nvar)         !< Explicit diffusion operator
+real(stm_real), intent (in)  :: conc_prev(ncell,nvar)                       !< Concentration at old time
+real(stm_real), intent (in)  :: area_lo_prev (ncell,nvar)                   !< Low side area at old time
+real(stm_real), intent (in)  :: area_hi_prev (ncell,nvar)                   !< High side area at old time 
+real(stm_real), intent (in)  :: disp_coef_lo (ncell,nvar)              !< Low side sediment dispersion coef. at old time
+real(stm_real), intent (in)  :: disp_coef_hi (ncell,nvar)              !< High side sediment dispersion coef. at old time
+real(stm_real), intent (in)  :: time                                   !< Current time
+real(stm_real), intent (in)  :: dx                                     !< Spacial step  
 
 !--- locals
 integer :: ivar
