97167c5ab60ba510344cfe37f9de088e0be46ac9
kuckao@gmail.com <kuckao@gmail.com@93266118-e052-11de-8dcd-9b777bee35ae>
2009-12-21 16:19:03 +0000

    move matrix solvers to a separate module.

--- trunk/stm/src/transport/diffusion.f90
+++ trunk/stm/src/transport/diffusion.f90
@@ -442,7 +442,7 @@ down_diag=0
 return
 end subroutine construct_diffusion_matrix
 !/////////////////////////////////////////////////
-!> Solvers the system of equations
+!> Solve the system of linear equations
 subroutine solve ( center_diag ,           &
                           up_diag,              &     
                           down_diag,            &
@@ -450,6 +450,8 @@ subroutine solve ( center_diag ,           &
                           conc,                 &
                           ncell,                &
                           nvar)
+
+use matrix_solver
                                                         
 ! ----- args
 use stm_precision
@@ -492,60 +494,5 @@ call tridi_solver ( center_diag ,           &
 
 return
 end subroutine solve
-!/////////////////////////////////////////////////
-                                 
-    pure subroutine tridi_solver ( center_diag ,           &
-                                      up_diag,              &     
-                                      down_diag,            &
-                                      right_hand_side,      &
-                                      conc,                 &
-                                      ncell,                &
-                                      nvar)
-    use stm_precision
 
-    ! ----- args
-
-    integer,intent (in) :: ncell                          !< Number of volumes 
-    integer, intent(in) :: nvar                           !< Number of variables 
-
-    real(stm_real),intent (in)  :: down_diag(ncell)       !< Values of the coefficients below diagonal in matrix
-    real(stm_real),intent (in)  :: center_diag(ncell)     !< Values of the coefficients at the diagonal in matrix
-    real(stm_real),intent (in)  :: up_diag(ncell)         !< Values of the coefficients above the diagonal in matrix
-    real(stm_real),intent (in)  :: right_hand_side(ncell) !< Values of the right hand side vector
-    real(stm_real),intent (out) :: conc(ncell)            !< Values of the computed solution
-
-    !---- Local 
-
-    integer :: ivar
-    real(stm_real) :: gam(ncell)
-    real(stm_real) :: bet
-
-
-
-    if(center_diag(1) == 0)then
-! todo: print and stop can not exist in pure subroutine
-!      print *, 'there is a problem in tridi-solver '
-!      stop
-    end if
-
-    bet = center_diag(1)
-    conc(1) = right_hand_side(1) / bet
-
-    do ivar=2, ncell
-      gam(ivar) = up_diag(ivar - 1) / bet
-      bet = center_diag(ivar) - down_diag(ivar) * gam(ivar)
-      if(bet == 0)then
-! todo: print can not exist in pure subroutine      
-!        print *, 'tridiagonal solver failed'
-      end if
-      conc(ivar) = (right_hand_side(ivar) - down_diag(ivar) * conc(ivar - 1)) / bet 
-    end do
-
-    do ivar= ncell-1, 1, -1
-      conc(ivar) = conc(ivar) - gam(ivar + 1) * conc(ivar + 1)
-    end do
-
-    return
-    end subroutine tridi_solver
-    
 end module diffusion    
