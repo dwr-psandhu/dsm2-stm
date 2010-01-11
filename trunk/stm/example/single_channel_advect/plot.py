4e54c9e23caef792bd0f52316590aebe01cff036
kuckao@gmail.com <kuckao@gmail.com@93266118-e052-11de-8dcd-9b777bee35ae>
2010-01-11 23:27:40 +0000

    organize parsing into function.

--- trunk/stm/example/single_channel_advect/plot.py
+++ trunk/stm/example/single_channel_advect/plot.py
@@ -3,89 +3,85 @@ import numpy as np
 import sys
 import re
 
-for arg in sys.argv:
-    print arg
-
-#x = np.linspace(xmin,xmax,n)
+# todo: read these file names from a input file or system arguments.
+datafile      = open('../../../stm/build/vs2008/example_single_channel/uniform_rectangular.dat', 'r')
+datafile_ref  = open('../../../stm/build/vs2008/example_single_channel/uniform_rectangular_ref.dat', 'r')
+#for arg in sys.argv:
+#    print arg
 
 xmin = 0
 xmax = 100.
 ymin = 0
 ymax = 6.
+v = [xmin,xmax,ymin,ymax] #axis range for plotting
 nx = 0
 nplot = 0 
-
-
-# todo: read these file names from a input file or system arguments.
-datafile      = open('../../../stm/build/vs2008/example_single_channel/uniform_rectangular.dat', 'r')
-datafile_ref  = open('../../../stm/build/vs2008/example_single_channel/uniform_rectangular_ref.dat', 'r')
-datalines     = datafile.readlines()
-datalines_ref = datafile_ref.readlines()
-dataraw=[]
-data=[]
-i=0
-
-# todo: rewrite below code into a function
-
-while i< len(datalines):
-
-    if "zone" in datalines[i]:
-        p = re.compile('i.*\= *(\d+)[, ].+\"(.+)\"')
-        ps = p.search(datalines[i])
-        
-        print ps.group(0)
-        print ps.group(1)
-        print ps.group(2)
-        
-        nx = int(ps.group(1))
-        nplot += 1 
-        
-        # read x y 
-        dataraw.append(line.split() for line in datalines[i+1:i+nx+1])
-        
-        # fast forward
-        i += nx
-
-    else:
-        i += 1
-        
 x=[]
 y=[]
 
-for set in dataraw:
-    # transpose to [(x1, x2,... xn), (y1, y2, ... yn)]
-    datatranspose =  zip(*set)
+def parse2xy(infile):
     
-    x.append(np.array(map(float,datatranspose[0]) ))
-    y.append(np.array(map(float,datatranspose[1]) ))
+    lines = infile.readlines()
+    i=0
+    nzone = 0
+    data = []
+    dataTransposed =[]
+    xc=[]
+    yc=[]
+    while i< len(lines):
     
+        if "zone" in lines[i]:
+    
+            p = re.compile('i.*\= *(\d+)[, ].+\"(.+)\"')            
+            ps = p.search(lines[i])
+            
+            nsize = int(ps.group(1))
+            nzone += 1 
+            
+            # read x y
+            data=[]
+            
+            # list of [['x0','y0'],['x1','y1'],...]
+            for line in lines[i+1:i+nsize+1]:
+            
+                data.append(line.split())
+                        
+            # transpose to [('x1','x2',...),('y1','y2',...)]
+            dataTransposed = zip(*data)
+            
+            xc.append(np.array(map(float,dataTransposed[0])))
+            yc.append(np.array(map(float,dataTransposed[1])))
+            
+            # fast forward to the end of this zone
+            i += nsize
+    
+        else:
+            i += 1
+
+    return xc,yc,nsize,nzone
 
 
 
-v = [xmin,xmax,ymin,ymax]
 
-def plot():
 
-    pl.figure(1)
+def plot(figureID,x,y,xr,yr,axisRange,outFile):
+
+    pl.figure(figureID)
     pl.xlabel('x (m)')
     pl.ylabel('concentration')    
-    pl.plot(x[0],y[0],'ro',x[0],y[0],'g')
-    pl.axis(v)
+    pl.plot(x,y,'ro',xr,yr,'g')
+    pl.axis(axisRange)
 
-    pl.savefig("test1.pdf", dpi=None, facecolor='w', edgecolor='w',
+    pl.savefig(outFile+".pdf", dpi=None, facecolor='w', edgecolor='w',
         orientation='portrait', papertype=None, format='pdf',
         transparent=False)
 
-    pl.figure(2)
-    pl.xlabel('x (m)')
-    pl.ylabel('concentration')
-    pl.plot(x[1],y[1],'ro',x[1],y[1],'g')
-    pl.axis(v)
 
-    pl.savefig("test2.pdf", dpi=None, facecolor='w', edgecolor='w',
-        orientation='portrait', papertype=None, format='pdf',
-        transparent=False)
 
-plot()
+x,y,nx,nplot = parse2xy(datafile)
+xr,yr,nx,nplot = parse2xy(datafile_ref)
+
+plot(1,x[3],y[3],xr[3],yr[3],v,'3')
+plot(2,x[6],y[6],xr[6],yr[6],v,'6')
 pl.show()
 
