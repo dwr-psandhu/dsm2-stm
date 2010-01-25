0181e085d1668bb461e0df576bbbbb0a4f6f47f2
kavehzamani <kavehzamani@93266118-e052-11de-8dcd-9b777bee35ae>
2010-01-25 23:05:43 +0000

    The missing parts of test replaced in test transport

---
+++ stm/test/transport/test_interior_diffusive_flux.F90
@@ -0,0 +1,90 @@
+!<license>
+!    Copyright (C) 1996, 1997, 1998, 2001, 2007, 2009 State of California,
+!    Department of Water Resources.
+!    This file is part of DSM2.
+!
+!    The Delta Simulation Model 2 (DSM2) is free software: 
+!    you can redistribute it and/or modify
+!    it under the terms of the GNU General Public License as published by
+!    the Free Software Foundation, either version 3 of the License, or
+!    (at your option) any later version.
+!
+!    DSM2 is distributed in the hope that it will be useful,
+!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!    GNU General Public License for more details.
+!
+!    You should have received a copy of the GNU General Public License
+!    along with DSM2.  If not, see <http://www.gnu.org/licenses>.
+!</license>
+
+!> todo: write tests for interior diffusive flux sobroutine
+!>@ingroup test
+module test_interior_diffusive_flux
+use diffusion
+use fruit
+use stm_precision
+
+contains
+
+subroutine test_interior_dif_flux_sub
+  use diffusion ! todo do we need it here?
+  implicit none
+  
+ integer,parameter :: ncell = 6 !< Number of cells
+integer,parameter  :: nvar = 1  !< Number of variables
+
+real(stm_real) :: diffusive_flux_interior_hi(ncell,nvar)      !< Explicit diffusive flux high side
+real(stm_real) :: diffusive_flux_interior_lo(ncell,nvar)      !< Explicit diffusive flux low side
+real(stm_real) :: conc_prev(ncell,nvar)                       !< Concentration at old time
+real(stm_real) :: area_lo_prev (ncell)                        !< Low side area at old time
+real(stm_real) :: area_hi_prev (ncell)                        !< High side area at old time 
+real(stm_real) :: disp_coef_lo_prev (ncell,nvar)              !< Low side constituent dispersion coef. at old time
+real(stm_real) :: disp_coef_hi_prev (ncell,nvar)              !< High side constituent dispersion coef. at old time
+real(stm_real) :: time                                        !< Current time
+real(stm_real) :: dx                                          !< Spatial step   
+
+conc_prev(:,1)  = (/300d0,305d0,320d0,330d0,340d0,350d0/)
+area_lo_prev(:) = (/100d0,98d0,96d0,94d0,92d0,90d0/)
+area_hi_prev(:) = (/98d0,96d0,94d0,92d0,90d0,88d0/)
+disp_coef_lo_prev(:,1) = (/0.9d0,0.92d0,0.94d0,0.96d0,0.98d0,1d0/)
+disp_coef_hi_prev(:,1) = (/0.92d0,0.94d0,0.96d0,0.98d0,1d0,1.02d0/)
+dx = 2.0d0 
+time =LARGEREAL ! todo do wee need this?
+ 
+call interior_diffusive_flux (diffusive_flux_interior_lo,  &
+                                    diffusive_flux_interior_hi,  &
+                                            conc_prev,        &
+                                            area_lo_prev,     &
+                                            area_hi_prev,     &
+                                            disp_coef_lo_prev,&  
+                                            disp_coef_hi_prev,&
+                                            ncell,            &
+                                            nvar,             &
+                                            time,             &
+                                            dx)
+                                            
+                                            
+!----diffusive_flux_interior_lo
+
+ 
+  call assertEquals (diffusive_flux_interior_lo(2,1),225.4d0 ,1d-9,"Error in diffusive_flux_interior_lo cell 2")
+  call assertEquals (diffusive_flux_interior_lo(3,1),676.8d0 ,1d-9,"Error in diffusive_flux_interior_lo cell 3")
+  call assertEquals (diffusive_flux_interior_lo(4,1),451.2d0 ,1d-9,"Error in diffusive_flux_interior_lo cell 4")
+  call assertEquals (diffusive_flux_interior_lo(5,1),450.8d0 ,1d-9,"Error in diffusive_flux_interior_lo cell 5")
+  call assertEquals (diffusive_flux_interior_lo(6,1),450d0 ,1d-9,"Error in diffusive_flux_interior_lo cell 6")
+  
+!----diffusive_flux_interior_hi
+
+  
+  call assertEquals (diffusive_flux_interior_hi(1,1),225.4d0 ,1d-9,"Error in diffusive_flux_interior_lo cell 1")
+  call assertEquals (diffusive_flux_interior_hi(2,1),676.8d0 ,1d-9,"Error in diffusive_flux_interior_lo cell 2")
+  call assertEquals (diffusive_flux_interior_hi(3,1),451.2d0 ,1d-9,"Error in diffusive_flux_interior_lo cell 3")
+  call assertEquals (diffusive_flux_interior_hi(4,1),450.8d0 ,1d-9,"Error in diffusive_flux_interior_lo cell 4")
+  call assertEquals (diffusive_flux_interior_hi(5,1),450d0 ,1d-9,"Error in diffusive_flux_interior_lo cell 5")
+  
+
+return
+end subroutine test_interior_dif_flux_sub
+
+end module
---
+++ stm/test/transport/test_matrix_solver.F90
@@ -0,0 +1,74 @@
+!<license>
+!    Copyright (C) 1996, 1997, 1998, 2001, 2007, 2009 State of California,
+!    Department of Water Resources.
+!    This file is part of DSM2.
+!
+!    The Delta Simulation Model 2 (DSM2) is free software: 
+!    you can redistribute it and/or modify
+!    it under the terms of the GNU General Public License as published by
+!    the Free Software Foundation, either version 3 of the License, or
+!    (at your option) any later version.
+!
+!    DSM2 is distributed in the hope that it will be useful,
+!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!    GNU General Public License for more details.
+!
+!    You should have received a copy of the GNU General Public License
+!    along with DSM2.  If not, see <http://www.gnu.org/licenses>.
+!</license>
+
+!> todo: write tests for matrix solver
+!>@ingroup test
+module test_matrix_solver
+
+use matrix_solver
+use fruit
+use stm_precision
+
+contains
+
+subroutine test_tridi_solver
+  use matrix_solver
+  implicit none
+  
+  integer,parameter :: ncell = 11                          !< Number of volumes 
+    
+    real(stm_real)  :: down_diag(ncell)       !< Values of the coefficients below diagonal in matrix
+    real(stm_real)  :: center_diag(ncell)     !< Values of the coefficients at the diagonal in matrix
+    real(stm_real)  :: up_diag(ncell)         !< Values of the coefficients above the diagonal in matrix
+    real(stm_real)  :: right_hand_side(ncell) !< Values of the right hand side vector
+    real(stm_real)  :: conc(ncell)            !< Values of the computed solution
+   
+ !--- Small numbers on center diag large numbers on up and down diag
+  
+ center_diag = (/0.17D0,0.18D0,0.19D0,0.2D0,0.21D0,0.22D0,0.23D0,0.24D0,0.25D0,0.26D0,0.27D0/)
+ down_diag = (/LARGEREAL,26.D0,31.D0,36.D0,41.D0,46.D0,51.D0,56.D0,61.D0,66.D0,71.D0/)
+ up_diag = (/36.D0,37.D0,38.D0,39.D0,40.D0,41.D0,42.D0,43.D0,44.D0,45.D0,LARGEREAL/)
+ right_hand_side = (/0.01D0,0.02D0,0.03D0,0.04D0,500.0D0,400.0D0,0.07D0,0.08D0,0.09D0,0.1D0,0.11D0/)
+
+call tridi_solver (center_diag,up_diag,down_diag,right_hand_side,conc,ncell)
+
+
+  call assertEquals (conc(1),613.689083199382D0,1d-9,"problem in solving cell num.1")
+  call assertEquals (conc(4),4.520833065491D0,1d-9,"problem in solving cell num.4")
+  call assertEquals (conc(11),-834.471026100105D0,1d-9,"problem in solving cell num.11")
+
+!--- Large numbers on center diag small numbers on up and down diag
+
+center_diag = (/17D0,18D0,19D0,20D0,21D0,22D0,23D0,24D0,25D0,26D0,27D0/)
+ down_diag = (/LARGEREAL,0.05d0,0.1d0,0.15d0,0.2d0,0.25d0,0.3d0,0.35d0,0.4d0,0.45d0,0.5d0/)
+ up_diag = (/0.1d0,0.2d0,0.3d0,0.4d0,0.5d0,0.6d0,0.7d0,0.8d0,0.9d0,1D0,LARGEREAL/)
+ right_hand_side = (/0.01D0,0.02D0,0.03D0,0.04D0,0.05d0,0.06D0,0.07D0,0.08D0,0.09D0,0.1D0,0.11D0/)
+
+call tridi_solver (center_diag,up_diag,down_diag,right_hand_side,conc,ncell)
+
+
+  call assertEquals (conc(1),0.000581809672D0,1d-9,"problem in solving cell num.1")
+  call assertEquals (conc(4),0.001942430407D0,1d-9,"problem in solving cell num.4")
+  call assertEquals (conc(11),0.004006798484D0,1d-9,"problem in solving cell num.11")
+
+return
+end subroutine test_tridi_solver
+
+end module
