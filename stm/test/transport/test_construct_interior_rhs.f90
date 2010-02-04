c185b186f075ffca35032d800f70b0f74f4813e9
kavehzamani <kavehzamani@93266118-e052-11de-8dcd-9b777bee35ae>
2010-02-04 22:42:59 +0000

    Added missing file and renamed file to lower case extension.

---
+++ stm/test/transport/test_construct_interior_rhs.f90
@@ -0,0 +1,143 @@
+!<license>
+!    Copyright (C) 1996, 1997, 1998, 2001, 2007, 2009 State of California,
+!    Department of Water Resources.
+!    This file is part of DSM2.
+!
+!    The Delta Simulation Model 2 (DSM2) is free software: 
+!    you can redistribute it and/or modify
+!    it under the terms of the GNU General Public License as published by
+!    the Free Software Foundation, either version 3 of the License, or
+!    (at your option) any later version.
+!
+!    DSM2 is distributed in the hope that it will be useful,
+!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!    GNU General Public License for more details.
+!
+!    You should have received a copy of the GNU General Public License
+!    along with DSM2.  If not, see <http://www.gnu.org/licenses>.
+!</license>
+
+!> todo: write tests for construct right hand side subroutine before imposing Boundary Conditions
+!>@ingroup test
+module test_construct_interior_r_h_s
+
+use diffusion 
+use fruit
+use stm_precision
+
+contains
+
+subroutine test_construct_interior_rhs
+  use diffusion 
+  implicit none
+  
+ integer,parameter :: ncell = 6 !< Number of cells
+integer,parameter :: nvar = 1 !< Number of variables
+
+real(stm_real) :: right_hand_side(ncell,nvar)                 !< The right hand side vector
+real(stm_real)  :: explicit_diffuse_op (ncell,nvar)            !< Explicit diffusion operator
+real(stm_real)  :: area_prev (ncell)                           !< Cell centered area at old time 
+real(stm_real)  :: conc_prev(ncell,nvar)                       !< Concentration at old time
+real(stm_real)  :: area_lo_prev (ncell)                        !< Low side area at old time
+real(stm_real)  :: area_hi_prev (ncell)                        !< High side area at old time 
+real(stm_real)  :: disp_coef_lo_prev (ncell,nvar)              !< Low side constituent dispersion coef. at old time
+real(stm_real)  :: disp_coef_hi_prev (ncell,nvar)              !< High side constituent dispersion coef. at old time
+real(stm_real)  :: time                                        !< Current time
+real(stm_real)  :: theta_stm                                   !< Explicitness coefficient; 0 is explicit, 0.5 Crank-Nicolson, 1 full implicit  
+real(stm_real)  :: dx                                          !< Spatial step  
+real(stm_real)  :: dt                                          !< Time step                                   
+real(stm_real)  :: diffusive_flux_boundary_lo (nvar)
+real(stm_real)  :: diffusive_flux_boundary_hi (nvar) 
+
+
+conc_prev(:,1) = (/300.0d0,305.0d0,320.0d0,330.0d0,340.0d0,350.0d0/)
+explicit_diffuse_op(:,1) = (/112.7d0,225.7d0,-112.8d0,-0.2d0,-0.4d0,-225.d0/)
+area_lo_prev(:) = (/100.0d0,98.0d0, 96.0d0,94.0d0,92.0d0,90.0d0/)
+area_hi_prev(:) = (/98.0d0, 96.0d0,94.0d0,92.0d0,90.0d0,88.0d0/)
+area_prev (:) = (/99d0,97d0,95d0,93d0,91d0,89d0/)
+diffusive_flux_boundary_lo(:) = zero
+diffusive_flux_boundary_hi(:) = zero
+dx=2.0d0
+dt=1.0d0
+theta_stm = 1.0d0
+disp_coef_lo_prev(:,1) = (/0.9d0,0.92d0,0.94d0,0.96d0,0.98d0,1.d0/)
+disp_coef_hi_prev(:,1) = (/0.92d0,0.94d0,0.96d0,0.98d0,1.d0,1.02d0/)
+time =LARGEREAL ! todo do wee need this?
+
+   !--theta =1 
+   
+call construct_right_hand_side( right_hand_side,   & 
+                                  explicit_diffuse_op,   & 
+                                  area_prev,             &
+                                  area_lo_prev,          &
+                                  area_hi_prev,          &
+                                  disp_coef_lo_prev,     &
+                                  disp_coef_hi_prev,     &
+                                  conc_prev,             &
+                                  theta_stm,             &
+                                  ncell,                 &
+                                  diffusive_flux_boundary_lo, &
+                                  diffusive_flux_boundary_hi, &
+                                  time,                  &
+                                  nvar,                  &  
+                                  dx,                    &
+                                  dt)
+                                  
+                                  
+  call assertEquals (right_hand_side(2,1),29585d0,1d-8,"Error in r_h_s vector 2 ,theta=1")
+  call assertEquals (right_hand_side(5,1),30940d0,1d-8,"Error in r_h_s vector 5 ,theta=1")
+   
+     !--theta =0.6 
+     theta_stm = 0.6d0 
+   
+call construct_right_hand_side( right_hand_side,   & 
+                                  explicit_diffuse_op,   & 
+                                  area_prev,             &
+                                  area_lo_prev,          &
+                                  area_hi_prev,          &
+                                  disp_coef_lo_prev,     &
+                                  disp_coef_hi_prev,     &
+                                  conc_prev,             &
+                                  theta_stm,             &
+                                  ncell,                 &
+                                  diffusive_flux_boundary_lo, &
+                                  diffusive_flux_boundary_hi, &
+                                  time,                  &
+                                  nvar,                  &  
+                                  dx,                    &
+                                  dt)
+                                  
+                                  
+  call assertEquals (right_hand_side(2,1),29675.28d0,1d-8,"Error in r_h_s vector 2 ,theta = 0.6")
+  call assertEquals (right_hand_side(5,1),30939.84d0,1d-8,"Error in r_h_s vector 5 ,theta = 0.6")
+  
+  !--theta =0.1 
+     theta_stm = 0.1d0 
+   
+call construct_right_hand_side( right_hand_side,   & 
+                                  explicit_diffuse_op,   & 
+                                  area_prev,             &
+                                  area_lo_prev,          &
+                                  area_hi_prev,          &
+                                  disp_coef_lo_prev,     &
+                                  disp_coef_hi_prev,     &
+                                  conc_prev,             &
+                                  theta_stm,             &
+                                  ncell,                 &
+                                  diffusive_flux_boundary_lo, &
+                                  diffusive_flux_boundary_hi, &
+                                  time,                  &
+                                  nvar,                  &  
+                                  dx,                    &
+                                  dt)
+                                  
+                                  
+  call assertEquals (right_hand_side(2,1),29788.13d0,1d-8,"Error in r_h_s vector 2 ,theta = 0.1")
+  call assertEquals (right_hand_side(5,1),30939.64d0,1d-8,"Error in r_h_s vector 5 ,theta = 0.1")
+
+
+return
+end subroutine test_construct_interior_rhs
+
+end module
