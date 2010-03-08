735c1fa36e6a72c22c7a9c9773139a2b25531dce
kavehzamani <kavehzamani@93266118-e052-11de-8dcd-9b777bee35ae>
2010-03-08 23:18:39 +0000

    remove prints, add documentation, add title, resolve todo:, replace pi

--- trunk/stm/src/transport/primitive_variable_conversion.f90
+++ trunk/stm/src/transport/primitive_variable_conversion.f90
@@ -17,39 +17,45 @@
 !    You should have received a copy of the GNU General Public License
 !    along with DSM2.  If not, see <http://www.gnu.org/licenses>.
 !</license>
-
+!> Module containing routines for converting mass to concentraion, and concentration to mass
+!>@ingroup transport
 module primitive_variable_conversion
 
 contains
 !> Convert conservative variables (ie mass) to primitive (concentration)
 pure subroutine cons2prim(conc,mass,area,nloc,nvar)
+
 use stm_precision
+
 implicit none
 real(stm_real),intent(out) :: conc(nloc,nvar)   !< concentration (converted from mass per unit length )
 real(stm_real),intent(in)  :: mass(nloc,nvar)   !< mass per unit length 
 real(stm_real),intent(in)  :: area(nloc)        !< area at conversion locations
 !--- args
-integer,intent(in)  :: nloc   !< Number of cells or faces
-integer,intent(in)  :: nvar   !< Number of variables
+integer,intent(in)  :: nloc                     !< Number of cells or faces
+integer,intent(in)  :: nvar                     !< Number of variables
 !--- locals
 integer :: ivar
 !-------------------
 do ivar = 1,nvar
     conc(:,ivar) = mass(:,ivar)/area
 end do
+
 return
 end subroutine
 
 !> Convert  primitive (concentration) to conservative variables (ie mass)
 pure subroutine prim2cons(mass,conc,area,nloc,nvar)
+
 use stm_precision
+
 implicit none
 real(stm_real),intent(out) :: mass(nloc,nvar)  !< mass per unit length (converted from concentration)
 real(stm_real),intent(in)  :: conc(nloc,nvar)  !< concentrations to convert
 real(stm_real),intent(in)  :: area(nloc)       !< area at conversion locations
 !--- args
-integer,intent(in)  :: nloc   !< Number of cells or faces
-integer,intent(in)  :: nvar   !< Number of variables
+integer,intent(in)  :: nloc                    !< Number of cells or faces
+integer,intent(in)  :: nvar                    !< Number of variables
 !--- locals
 integer :: ivar
 !-------------------
@@ -57,6 +63,7 @@ integer :: ivar
 do ivar = 1,nvar
     mass(:,ivar) = conc(:,ivar)*area
 end do
+
 return
 end subroutine
 
--- trunk/stm/test/example/test_example_initial_conditions.f90
+++ trunk/stm/test/example/test_example_initial_conditions.f90
@@ -22,10 +22,13 @@
 
 
 subroutine test_example_initial_conditions
+
 use stm_precision
 use example_initial_conditions
 use fruit
+
 implicit none
+
 integer, parameter :: nloc = 100
 real(stm_real) :: vals(nloc,2)
 real(stm_real), parameter  :: dx = ten
@@ -38,22 +41,25 @@ real(stm_real) :: offline_calc
 real(stm_real) :: cell_calc41
 real(stm_real) :: cell_calc61
 character(LEN=32) :: message
-integer :: icell
 
+integer :: icell
 
 ! Check symmetry and two constituents
 call fill_gaussian(vals(:,1),nloc,origin,dx,center1,sd)
 
 call fill_gaussian(vals(:,2),nloc,origin,dx,center2,sd)
 
-
 ! test the center cell for each plume. 
 ! The cell edges lo/hi are half a dx = 1/8 of sd from center
 ! so use tabulated values of the cdf at the mean +/- 1/8*sigma
 offline_calc = 0.09947645
-cell_calc41 = vals(41,1)*sqrt(two*acos(-one)*sd*sd)  !todo: make pi a constant
+
+cell_calc41 = vals(41,1)*sqrt(two*pi*sd*sd)  
+
 call assertEquals(cell_calc41,offline_calc,epsilon,"Integral gaussian in cell 41")
+
 cell_calc61 = vals(61,2)
+
 call assertEquals(cell_calc41,cell_calc61,"Symmetry of integral gaussian in cells 41, 61")
 
 call fill_gaussian(vals(:,1),nloc,origin,dx/two,center1,sd)
@@ -64,23 +70,27 @@ call assertEquals(vals(41,1),half,"Discontinuity IC halfway in cell (41)")
 call assertEquals(vals(41,2),zero,"Discontinuity IC halfway in cell (41) -- non-discontinuous constituent")
 call assertEquals(vals(61,2),half,"Discontinuity IC halfway in cell (61)")
 call assertEquals(vals(61,1),one,"Discontinuity IC halfway in cell (61) -- non-discontinuous constituent")
+
 do icell = 1,40
     message = "Discontinuity IC, lo cells (constituent 1)"
     call assertEquals(vals(icell,1),zero, message)
     message = "Discontinuity IC, lo cells (constituent 2)"    
     call assertEquals(vals(icell,2),zero, message)
 end do
+
 do icell = 42,60
     message = "Discontinuity IC, middle (constituent 1)"
     call assertEquals(vals(icell,1),one, message)
     message = "Discontinuity IC, middle (constituent 2)"    
     call assertEquals(vals(icell,2),zero, message)
 end do
+
 do icell = 62,100
     message = "Discontinuity IC, hi cells (constituent 1)"
     call assertEquals(vals(icell,1),one, message)
     message = "Discontinuity IC, hi cells (constituent 2)"
     call assertEquals(vals(icell,2),one, message)
 end do
+
 return
 end subroutine
