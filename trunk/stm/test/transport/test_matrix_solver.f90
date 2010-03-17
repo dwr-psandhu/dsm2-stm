220a89b98636c40742cd95546a9e0ecd04d082b1
kavehzamani <kavehzamani@93266118-e052-11de-8dcd-9b777bee35ae>
2010-03-17 18:28:11 +0000

    test_matrix_solver in generalized for multi constituent

--- trunk/stm/test/transport/test_matrix_solver.f90
+++ trunk/stm/test/transport/test_matrix_solver.f90
@@ -34,41 +34,50 @@ use matrix_solver
 
 implicit none
   
-  integer,parameter :: ncell = 11             !< Number of volumes 
+  integer,parameter :: ncell = 11             !< number of volumes 
+  integer,parameter :: nvar = 2               !< number of constituent 
     
-    real(stm_real)  :: down_diag(ncell)       !< Values of the coefficients below diagonal in matrix
-    real(stm_real)  :: center_diag(ncell)     !< Values of the coefficients at the diagonal in matrix
-    real(stm_real)  :: up_diag(ncell)         !< Values of the coefficients above the diagonal in matrix
-    real(stm_real)  :: right_hand_side(ncell) !< Values of the right hand side vector
-    real(stm_real)  :: conc(ncell)            !< Values of the computed solution
+    
+    real(stm_real)  :: down_diag(ncell,nvar)       !< Values of the coefficients below diagonal in matrix
+    real(stm_real)  :: center_diag(ncell,nvar)     !< Values of the coefficients at the diagonal in matrix
+    real(stm_real)  :: up_diag(ncell,nvar)         !< Values of the coefficients above the diagonal in matrix
+    real(stm_real)  :: right_hand_side(ncell,nvar) !< Values of the right hand side vector
+    real(stm_real)  :: conc(ncell,nvar)            !< Values of the computed solution
+    
+!---- local
+integer :: ivar    
+
    
  !--- Small numbers on center diag large numbers on up and down diag
   
- center_diag = (/0.17D0,0.18D0,0.19D0,0.2D0,0.21D0,0.22D0,0.23D0,0.24D0,0.25D0,0.26D0,0.27D0/)
- down_diag = (/LARGEREAL,26.D0,31.D0,36.D0,41.D0,46.D0,51.D0,56.D0,61.D0,66.D0,71.D0/)
- up_diag = (/36.D0,37.D0,38.D0,39.D0,40.D0,41.D0,42.D0,43.D0,44.D0,45.D0,LARGEREAL/)
- right_hand_side = (/0.01D0,0.02D0,0.03D0,0.04D0,500.0D0,400.0D0,0.07D0,0.08D0,0.09D0,0.1D0,0.11D0/)
+ center_diag (:,2) = (/0.17D0,0.18D0,0.19D0,0.2D0,0.21D0,0.22D0,0.23D0,0.24D0,0.25D0,0.26D0,0.27D0/)
+ down_diag(:,2)  = (/LARGEREAL,26.D0,31.D0,36.D0,41.D0,46.D0,51.D0,56.D0,61.D0,66.D0,71.D0/)
+ up_diag (:,2) = (/36.D0,37.D0,38.D0,39.D0,40.D0,41.D0,42.D0,43.D0,44.D0,45.D0,LARGEREAL/)
+ right_hand_side(:,2)  = (/0.01D0,0.02D0,0.03D0,0.04D0,500.0D0,400.0D0,0.07D0,0.08D0,0.09D0,0.1D0,0.11D0/)
 
-call tridi_solver (center_diag,up_diag,down_diag,right_hand_side,conc,ncell)
+do ivar = 1,nvar
+  call tridi_solver (center_diag(:,ivar),up_diag(:,ivar),down_diag(:,ivar),right_hand_side(:,ivar),conc(:,ivar),ncell)
+end do
 
-
-  call assertEquals (conc(1),613.689083199382D0,1d-9,"problem in solving cell num.1")
-  call assertEquals (conc(4),4.520833065491D0,1d-9,"problem in solving cell num.4")
-  call assertEquals (conc(11),-834.471026100105D0,1d-9,"problem in solving cell num.11")
-
+  call assertEquals (conc(1,2),613.689083199382D0,1d-9,"problem in solving cell num.1")
+  call assertEquals (conc(4,2),4.520833065491D0,1d-9,"problem in solving cell num.4")
+  call assertEquals (conc(11,2),-834.471026100105D0,1d-9,"problem in solving cell num.11")
+  
 !--- Large numbers on center diag small numbers on up and down diag
 
-    center_diag = (/17D0,18D0,19D0,20D0,21D0,22D0,23D0,24D0,25D0,26D0,27D0/)
-    down_diag = (/LARGEREAL,0.05d0,0.1d0,0.15d0,0.2d0,0.25d0,0.3d0,0.35d0,0.4d0,0.45d0,0.5d0/)
-    up_diag = (/0.1d0,0.2d0,0.3d0,0.4d0,0.5d0,0.6d0,0.7d0,0.8d0,0.9d0,1D0,LARGEREAL/)
-    right_hand_side = (/0.01D0,0.02D0,0.03D0,0.04D0,0.05d0,0.06D0,0.07D0,0.08D0,0.09D0,0.1D0,0.11D0/)
+    center_diag(:,2) = (/17D0,18D0,19D0,20D0,21D0,22D0,23D0,24D0,25D0,26D0,27D0/)
+    down_diag(:,2) = (/LARGEREAL,0.05d0,0.1d0,0.15d0,0.2d0,0.25d0,0.3d0,0.35d0,0.4d0,0.45d0,0.5d0/)
+    up_diag(:,2) = (/0.1d0,0.2d0,0.3d0,0.4d0,0.5d0,0.6d0,0.7d0,0.8d0,0.9d0,1D0,LARGEREAL/)
+    right_hand_side(:,2) = (/0.01D0,0.02D0,0.03D0,0.04D0,0.05d0,0.06D0,0.07D0,0.08D0,0.09D0,0.1D0,0.11D0/)
 
-call tridi_solver (center_diag,up_diag,down_diag,right_hand_side,conc,ncell)
+call tridi_solver (center_diag(:,2),up_diag(:,2),down_diag(:,2),right_hand_side(:,2),conc(:,2),ncell)
 
 
-  call assertEquals (conc(1),0.000581809672D0,1d-9,"problem in solving cell num.1")
-  call assertEquals (conc(4),0.001942430407D0,1d-9,"problem in solving cell num.4")
-  call assertEquals (conc(11),0.004006798484D0,1d-9,"problem in solving cell num.11")
+  call assertEquals (conc(1,2),0.000581809672D0,1d-9,"problem in solving cell num.1")
+  call assertEquals (conc(4,2),0.001942430407D0,1d-9,"problem in solving cell num.4")
+  call assertEquals (conc(11,2),0.004006798484D0,1d-9,"problem in solving cell num.11")
+
+   
 
 return
 end subroutine test_tridi_solver
