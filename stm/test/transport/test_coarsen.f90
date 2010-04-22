b9bcb3a3e637f71efa4ce70bcfddba5770823f84
kavehzamani <kavehzamani@93266118-e052-11de-8dcd-9b777bee35ae>
2010-04-22 20:14:49 +0000

    test fro coarsen subroutine

---
+++ stm/test/transport/test_coarsen.f90
@@ -0,0 +1,75 @@
+!<license>
+!    Copyright (C) 1996, 1997, 1998, 2001, 2007, 2009 State of California,
+!    Department of Water Resources.
+!    This file is part of DSM2.
+!
+!    The Delta Simulation Model 2 (DSM2) is free software: 
+!    you can redistribute it and/or modify
+!    it under the terms of the GNU General Public License as published by
+!    the Free Software Foundation, either version 3 of the License, or
+!    (at your option) any later version.
+!
+!    DSM2 is distributed in the hope that it will be useful,
+!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!    GNU General Public License for more details.
+!
+!    You should have received a copy of the GNU General Public License
+!    along with DSM2.  If not, see <http://www.gnu.org/licenses>.
+!</license>
+
+!> test coarsenig subroutine subroutine
+!>@ingroup test
+module test_coarsen
+
+use fruit
+use stm_precision
+
+contains
+
+subroutine test_coarsen_sub
+
+use test_single_channel_advection
+
+implicit none
+  
+
+!---arg
+integer :: ncell_coarse
+integer,parameter :: ncell_fine = 6
+integer,parameter :: nvar = 2
+real(stm_real) :: fine_data(ncell_fine,nvar)
+real(stm_real), allocatable :: coarse_data(:,:)
+!---- local
+ 
+ncell_coarse = 6
+allocate (coarse_data(ncell_coarse,nvar))
+fine_data(:,1) = (/1:6/)
+fine_data(:,2) = (/11:16/)
+
+  call coarsen(coarse_data,fine_data,ncell_fine,ncell_coarse, nvar)
+  call assertEquals (coarse_data,fine_data,1d-9,"error in conversion, coarsening factor equal to 1!")
+
+deallocate(coarse_data)
+
+ncell_coarse=4
+allocate (coarse_data(ncell_coarse,nvar))
+   call coarsen(coarse_data,fine_data,ncell_fine,ncell_coarse, nvar)
+!  todo: it should yeild an error
+
+deallocate(coarse_data)
+
+ncell_coarse=3
+allocate (coarse_data(ncell_coarse,nvar))
+   call coarsen(coarse_data,fine_data,ncell_fine,ncell_coarse, nvar)
+      
+
+  call assertEquals (coarse_data(:,1),(/2,5/),1d-9,"error in conversion, coarsening constituent 1")
+  call assertEquals (coarse_data(:,2),(/12,15/),1d-9,"error in conversion, coarsening constituent 2")
+  
+  deallocate (coarse_data)
+
+return
+end subroutine test_coarsen_sub
+
+end module
