18ded22713bd56bd891e045b4b86c2cdeb7ade80
kavehzamani <kavehzamani@93266118-e052-11de-8dcd-9b777bee35ae>
2010-10-06 15:26:04 +0000

    The conflict resolved

--- stm/test/transport/test_advection_convergence_tidal.f90
+++ stm/test/transport/test_advection_convergence_tidal.f90
@@ -32,6 +32,11 @@ real(stm_real),parameter :: depth = 16.0d0                 !< Channel depth in m
 real(stm_real),parameter :: sec_per_hr = 60.d0*60.d0       !< Convert factor of hour to second 
 real(stm_real),parameter :: m2_period = 12.4d0*sec_per_hr  !< M2 tidal period 
 real(stm_real),parameter :: freq=two*pi/m2_period          !< Frequency of tidal oscillation
+real(stm_real),parameter :: dye_length = domain_length/two
+real(stm_real),parameter :: dye_center = domain_length/two
+integer, parameter  :: nconc = 2                           !< Number of constituents
+integer, parameter  :: nstep_base = 64                     !< Number of time steps in finer discritization
+integer, parameter  :: nx_base    = 256                    !< Number of spatial discritization in finer mesh 
 
 
 contains
@@ -61,36 +66,40 @@ integer, parameter  :: nconc = 2                         !< Number of constituen
 integer, parameter  :: nstep_base = 512                !< Number of time steps in finer discritization
 integer, parameter  :: nx_base    = 1024              !< Number of spatial discritization in finer mesh 
 logical :: verbose
+
 logical :: detail_printout=.true.
 real(stm_real), parameter :: total_time = m2_period      !< total time of the test
 real(stm_real), parameter :: start_time = zero           !< starts at zero
 real(stm_real) :: fine_initial_condition(nx_base,nconc)  !< initial condition at finest resolution
-real(stm_real) :: fine_solution(nx_base,nconc)           !< reference solution at finest resolution
+real(stm_real) :: fine_solution(nx_base,nconc)            !< reference solution at finest resolution
 real(stm_real),parameter :: ic_center = domain_length/two  !< Center of initial condition
 real(stm_real) :: solution_center = ic_center             !< Center of final solution 
-real(stm_real),parameter :: ic_gaussian_sd = domain_length/32.d0   !< Standard deviation of initial values 
+real(stm_real),parameter :: ic_gaussian_sd = domain_length/sixteen   !< Standard deviation of initial values 
 real(stm_real) :: solution_gaussian_sd = ic_gaussian_sd !< Standard deviation of final values
 real(stm_real),parameter :: dye_length = domain_length/3.0d0
 
-character(LEN=*),parameter :: label = "advection_tidal"      
+character(LEN=64) :: label 
+ 
 tidal_hydro=> tidal_flow
 advection_boundary_flux => zero_advective_flux !todo: move this so it isn't hardwired
 boundary_diffusion_flux => no_diffusion_flux
 boundary_diffusion_matrix => no_diffusion_matrix
 compute_source => no_source
+const_dispersion = zero
+
+label = 'advection_tidal_gaussian' 
 
 !> load the initial values and reference final values to feed the test routine
-call initial_fine_solution_tidal(fine_initial_condition, &
-                                 fine_solution,          &
-                                 nx_base,                &
-                                 nconc,                  &
-                                 origin,                 &
-                                 domain_length,          &
-                                 ic_gaussian_sd,         &
-                                 solution_gaussian_sd,   &
-                                 ic_center,              &
-                                 solution_center,        &
-                                 dye_length)
+call initial_fine_solution_tidal_gaussian(fine_initial_condition, &
+                                          fine_solution,          &
+                                          nx_base,                &
+                                          nconc,                  &
+                                          origin,                 &
+                                          domain_length,          &
+                                          ic_gaussian_sd,         &
+                                          solution_gaussian_sd,   &
+                                          ic_center,              &
+                                          solution_center)
 
 
 !> The general subroutine which gets the fine initial and reference values from the privious subroutine and 
@@ -111,27 +120,53 @@ call test_convergence(label,                  &
                       nx_base,                &
                       nconc,                  &
                       verbose,                &
-                      detail_printout)
+                      detail_printout=.true.)
+                      
+label = "advection_tidal_sinusoidal" 
+!> load the initial values and reference final values to feed the test routine
+call initial_fine_solution_tidal_sinusoidal(fine_initial_condition, &
+                                            fine_solution,          &
+                                            nx_base,                &
+                                            nconc,                  &
+                                            domain_length,          &
+                                            dye_center,             &
+                                            dye_length)
+
+!> The general subroutine which gets the fine initial and reference values from the privious subroutine and 
+!> compute the norms, after each step coarsen the values and repeat computation.
+!> at the end  calculates the ratio of the norms and prints a log 
+call test_convergence(label,                  &
+                      tidal_hydro ,           &
+                      zero_advective_flux,    &
+                      no_diffusion_flux,      &
+                      no_diffusion_matrix,    &
+                      no_source,              &
+                      domain_length,          &
+                      total_time,             &
+                      start_time,             &
+                      fine_initial_condition, &
+                      fine_solution,          &            
+                      nstep_base,             &
+                      nx_base,                &
+                      nconc,                  &
+                      verbose,                &
+                      detail_printout=.true.)
 
 
 
-return
 end subroutine
 !-------------------------------------------
-!> Generates a fine initial and final solution of mass
-subroutine initial_fine_solution_tidal(fine_initial_condition, &
-                                       fine_solution,          &
-                                       nx_base,                &
-                                       nconc,                  &
-                                       origin,                 &
-                                       domain_length,          &
-                                       ic_gaussian_sd,         &
-                                       solution_gaussin_sd,    &
-                                       ic_center,              &
-                                       solution_center,        &
-                                       dye_length)
-                                       
-
+!> Generates a fine initial and final solution of a Gaussian  mass distribution 
+subroutine initial_fine_solution_tidal_gaussian(fine_initial_condition, &
+                                                fine_solution,          &
+                                                nx_base,                &
+                                                nconc,                  &
+                                                origin,                 &
+                                                domain_length,          &
+                                                ic_gaussian_sd,         &
+                                                solution_gaussin_sd,    &
+                                                ic_center,              &
+                                                solution_center)
 
 use gaussian_init_boundary_condition
 use stm_precision
@@ -148,7 +183,7 @@ real(stm_real),intent(in)  :: ic_gaussian_sd                        !< Standard 
 real(stm_real),intent(in)  :: solution_gaussin_sd                   !< Standard deviation of solution 
 real(stm_real),intent(in)  :: origin                                !< Left hand side of the channel
 real(stm_real),intent(in)  :: domain_length                         !< Domain length
-real(stm_real),intent(in)  :: dye_length                            !< length of cosine shape mass in the middle of the domain
+
 !----local
 real(stm_real):: dx
 real(stm_real):: xposition(nx_base)
@@ -157,28 +192,71 @@ integer :: icell
 dx = domain_length/nx_base
 
 !todo: remove these gaussian
-!call fill_gaussian(fine_initial_condition(:,1),nx_base,origin,dx, &
-!                   ic_center,ic_gaussian_sd,0.1d0)
-!call fill_gaussian(fine_initial_condition(:,2),nx_base,origin,dx, &
-!                   ic_center,ic_gaussian_sd,one)
-!
-!call fill_gaussian(fine_solution(:,1),nx_base,origin,dx, &
-!                   solution_center,solution_gaussin_sd,0.1d0)
-!call fill_gaussian(fine_solution(:,2),nx_base,origin,dx, &
-!                   solution_center,solution_gaussin_sd,one)
+call fill_gaussian(fine_initial_condition(:,1),nx_base,origin,dx, &
+                   ic_center,ic_gaussian_sd,0.1d0)
+call fill_gaussian(fine_initial_condition(:,2),nx_base,origin,dx, &
+                   ic_center,ic_gaussian_sd,one)
 
-do icell=1,nx_base
-    xposition(icell)=(dble(icell)-half)*dx
-    if ((xposition(icell)>(domain_length*half + dye_length*half)).or.(xposition(icell)<(domain_length*half - dye_length*half))) then
-        fine_initial_condition(icell,1)=zero
-    else
-        fine_initial_condition(icell,1)=cos((xposition(icell) - domain_length*half)*pi/dye_length)
-    end if
-end do
+call fill_gaussian(fine_solution(:,1),nx_base,origin,dx, &
+                   solution_center,solution_gaussin_sd,0.1d0)
+call fill_gaussian(fine_solution(:,2),nx_base,origin,dx, &
+                   solution_center,solution_gaussin_sd,one)
 
 fine_initial_condition(:,2)=fine_initial_condition(:,1)
 fine_solution = fine_initial_condition
 
+return
+end subroutine
+
+!-------------------------------------------
+!> Generates a fine initial and final solution of a sinusoidal  mass distribution 
+subroutine initial_fine_solution_tidal_sinusoidal(fine_initial_condition, &
+                                                  fine_solution,          &
+                                                  nx_base,                &
+                                                  nconc,                  &
+                                                  domain_length,          &
+                                                  dye_center,             &
+                                                  dye_length)
+                                       
+
+
+use gaussian_init_boundary_condition
+use stm_precision
+
+implicit none
+
+integer,intent(in) :: nconc 
+integer,intent(in) :: nx_base 
+real(stm_real),intent(out) :: fine_initial_condition(nx_base,nconc) !< initial condition at finest resolution
+real(stm_real),intent(out) :: fine_solution(nx_base,nconc)          !< reference solution at finest resolution
+real(stm_real),intent(in)  :: domain_length                         !< Domain length
+real(stm_real),intent(in)  :: dye_center                            !< center of sinusoidal mass
+real(stm_real),intent(in)  :: dye_length                            !< length of mass at the middle of the domain
+!----local
+real(stm_real):: dx
+real(stm_real):: xposition
+real(stm_real):: x_lo
+real(stm_real):: x_hi
+integer :: icell
+
+dx = domain_length/nx_base
+
+do icell=1,nx_base
+  x_lo     = (dble(icell)-one)*dx
+  xposition= (dble(icell)-half)*dx 
+  x_hi     = (dble(icell))*dx
+  if (( x_lo > (dye_center + dye_length*half)) .or. & 
+      ( x_hi < (dye_center - dye_length*half))  ) then
+        fine_initial_condition(icell,1) = zero
+  else
+        fine_initial_condition(icell,1)= one + &
+            (dye_length*half/pi)*(sin((x_hi - dye_center)*two*pi/dye_length) - &
+                                  sin((x_lo-dye_center)*two*pi/dye_length))/dx
+  end if 
+end do
+
+fine_initial_condition(:,2)=fine_initial_condition(:,1)
+fine_solution = fine_initial_condition
 
 return
 end subroutine
@@ -194,6 +272,61 @@ subroutine tidal_flow(flow,    &
                       time,  &
                       dx,      &
                       dt)
+                      
+use stm_precision
+
+implicit none
+integer, intent(in) :: ncell                   !< number of cells
+real(stm_real), intent(in)  :: time            !< time of request
+real(stm_real), intent(in)  :: dx              !< spatial step 
+real(stm_real), intent(in)  :: dt              !< time step 
+real(stm_real), intent(out) :: flow(ncell)     !< cell centered flow
+real(stm_real), intent(out) :: flow_lo(ncell)  !< lo face flow
+real(stm_real), intent(out) :: flow_hi(ncell)  !< hi face flow
+real(stm_real), intent(out) :: area(ncell)     !< cell center area
+real(stm_real), intent(out) :: area_lo(ncell)  !< area lo face
+real(stm_real), intent(out) :: area_hi(ncell)  !< area hi face
+
+!--- local
+real(stm_real) :: half_time
+real(stm_real) :: big_b 
+real(stm_real) :: big_a 
+real(stm_real) :: vel_lo
+real(stm_real) :: vel_hi
+real(stm_real) :: vel
+integer :: icell
+!todo: check this
+half_time = time - half*dt
+big_b = freq/sqrt(gravity*depth)
+big_a = amplitude* sqrt(gravity*depth)/(depth*cos(big_b*domain_length))
+
+! width is assumed to be equal to 1 meter 
+do icell = 1,ncell  
+  area(icell)    = depth + amplitude * cos(big_b*(domain_length-(dble(icell)-half)*dx))/cos(big_b*domain_length)*cos(freq*time)  
+  area_lo(icell) = depth + amplitude * cos(big_b*(domain_length-(dble(icell-1)*dx)))   /cos(big_b*domain_length)*cos(freq*half_time)  
+  area_hi(icell) = depth + amplitude * cos(big_b*(domain_length-(dble(icell)*dx)))     /cos(big_b*domain_length)*cos(freq*half_time)  
+  vel_lo = big_a*sin(big_b*(domain_length - (dble(icell-1)*dx)) )*sin(freq*half_time)
+  vel_hi = big_a*sin(big_b*(domain_length - (dble(icell)*dx  )) )*sin(freq*half_time)
+  vel    = big_a*sin(big_b*(domain_length - ((dble(icell)-half)*dx)))*sin(freq*time)
+  flow(icell)    = area(icell)*vel
+  flow_lo(icell) = area_lo(icell)*vel_lo
+  flow_hi(icell) = area_hi(icell)*vel_hi
+end do  
+
+return
+end subroutine 
+
+!> tidal flow and area for a rectangular basin with periodic forcing
+subroutine tidal_flow_cell_average(flow,    &
+                                   flow_lo, &
+                                   flow_hi, &
+                                   area,    &
+                                   area_lo, &
+                                   area_hi, &
+                                   ncell,   &
+                                   time,    &
+                                   dx,      &
+                                   dt)
                       
 use stm_precision
 
@@ -237,6 +370,7 @@ end do
 
 return
 end subroutine 
+
 
 end module
 
