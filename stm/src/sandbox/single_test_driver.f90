b1efa74858aad5a126df788e484941be25d76890
eli-sava@pacbell.net <eli-sava@pacbell.net@93266118-e052-11de-8dcd-9b777bee35ae>
2010-12-06 15:54:25 +0000

    Added driver for a single test.

---
+++ stm/src/sandbox/single_test_driver.f90
@@ -0,0 +1,42 @@
+!<license>
+!    Copyright (C) 1996, 1997, 1998, 2001, 2007, 2009 State of California,
+!    Department of Water Resources.
+!    This file is part of DSM2.
+!
+!    The Delta Simulation Model 2 (DSM2) is free software: 
+!    you can redistribute it and/or modify
+!    it under the terms of the GNU General Public License as published by
+!    the Free Software Foundation, either version 3 of the License, or
+!    (at your option) any later version.
+!
+!    DSM2 is distributed in the hope that it will be useful,
+!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!    GNU General Public License for more details.
+!
+!    You should have received a copy of the GNU General Public License
+!    along with DSM2. If not, see <http://www.gnu.org/licenses>.
+!</license>
+
+!> Main program unit for testing teransport
+!>@ingroup test
+program single_test_driver
+
+use fruit
+
+
+use test_convergence_transport_uniform
+!use test_advection_reaction_tidal
+!use test_zoppou_advection_dispersion
+
+implicit none
+
+logical :: verbose = .true.
+
+call init_fruit
+
+call test_converge_transport_uniform(verbose)
+call fruit_summary
+
+pause
+end program 
