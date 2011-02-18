43c1da87e9e60edc89ee51e33a61b0f366c2aa70
kavehzamani <kavehzamani@93266118-e052-11de-8dcd-9b777bee35ae>
2011-02-18 17:59:33 +0000

    Compilable version added

--- stm/src/sediment/sediment_variables_statics.f90
+++ stm/src/sediment/sediment_variables_statics.f90
@@ -25,6 +25,7 @@
 !>@ingroup sediment
 module sediment_variables
     use stm_precision
+    integer :: ncell        !< Number of computational cells
     
     !> Example of variables to define, where the variable is constant.
     !> It is just a number    
@@ -34,76 +35,46 @@ module sediment_variables
     !> Example of variables to define, where the variable is a function of space only. 
     real(stm_real),save,allocatable :: manning_n(:)  !< Just function of space
 !todo: we need to add the other variables as in the table
-
-    !> Example of variables to define, where the variable is a function of both space and time.    
-    real(stm_real),save,allocatable :: flow_hi(:,:)  !< Function of time and space
-!todo: we need to add the other variables as in the table    
+    
+    
     
     contains
     
-    !> Allocate the state variables consistently
-    !> including concentration and hydrodynamics.
+     
     !> Initial value is LARGEREAL
-    subroutine allocate_state(a_ncell,a_nvar)
+    subroutine set_sediment_constant
         use error_handling
         implicit none
-        character(LEN=128) :: message
-        integer :: istat = 0
-        integer, intent(in) :: a_ncell !< Number of requested cells
-        integer, intent(in) :: a_nvar  !< Number of constituents
-        ncell = a_ncell
-        nvar  = a_nvar
-        write(message,*)"Could not allocate state variable. " //&
-         "This could be due to allocating several times in " // &
-         "a row without deallocating (memory leak)"
         
-        allocate(conc(ncell,nvar), conc_prev(ncell,nvar), stat = istat)
-        if (istat .ne. 0 )then
-           call stm_fatal(message)
-        end if
-        conc      = LARGEREAL  ! absurd value helps expose bugs  
-        conc_prev = LARGEREAL
-        allocate(mass(ncell,nvar), mass_prev(ncell,nvar),stat = istat)
-        if (istat .ne. 0 )then
-           call stm_fatal(message)
-        end if
-        mass      = LARGEREAL  ! absurd value helps expose bugs  
-        mass_prev = LARGEREAL       
-        allocate(area(ncell), area_prev(ncell), area_lo(ncell), area_hi(ncell), &
-                 area_lo_prev(ncell), area_hi_prev(ncell),stat = istat)
-        if (istat .ne. 0 )then
-           call stm_fatal(message)
-        end if
-        area      = LARGEREAL
-        area_prev = LARGEREAL
-        area_lo   = LARGEREAL
-        area_hi   = LARGEREAL
-        area_lo_prev   = LARGEREAL
-        area_hi_prev   = LARGEREAL
-        
-        allocate(flow(ncell),flow_lo(ncell), flow_hi(ncell),stat = istat)
-        if (istat .ne. 0 )then
-           call stm_fatal(message)
-        end if
-        flow      = LARGEREAL
-        flow_lo   = LARGEREAL
-        flow_hi   = LARGEREAL
+        real(stm_real) :: g_acceleration  
+       
+        g_acceleration = LARGEREAL
+       
         return
     end subroutine
     
-    !> Deallocate the state variables
-    !> including concentration and hydrodynamics
-    !> and reset ncell and nvar to zero.
-    subroutine deallocate_state
+    
+    subroutine allocate_sediment_static(ncell)
+        use error_handling
         implicit none
+        integer,intent(in):: ncell    !<Number of cells
+                   
+       allocate(manning_n(ncell))
+       
+        return
+    end subroutine
+    
+    
+    !> Deallocate the sediment static variable
+    !> including manning's n and width
+    !> and reset ncell and nvar to zero.
+    subroutine deallocate_sediment_static
+        implicit none
+
         ncell = 0
-        nvar  = 0
-        deallocate(conc, conc_prev, mass, mass_prev)
-        deallocate(area)
-        deallocate(area_prev)
-        deallocate(area_lo,area_hi)
-        deallocate(area_lo_prev, area_hi_prev)
-        deallocate(flow, flow_lo, flow_hi)
+      
+        deallocate(manning_n)
+      
         return
     end subroutine
 
