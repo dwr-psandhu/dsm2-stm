ff1a5db9d206d74f847c406e07962d53802cbc24
kavehzamani <kavehzamani@93266118-e052-11de-8dcd-9b777bee35ae>
2011-02-26 03:43:01 +0000

    Input structure set, there are many questions and todo(s) inside which need careful attention

--- stm/src/sediment/sediment_variable_data.f90
+++ stm/src/sediment/sediment_variable_data.f90
@@ -52,15 +52,23 @@ module suspended_sediment_variable
  abstract interface 
        !> Get spacial varible data (Manning's n and  for sediment module.
        !> This data might be calculated from a function or provided by a subroutine
-       subroutine spatial_data_sediment_if
-                                    !< Number of cells (in)
-                                    !< Time of request (in) (do we need this for future extension?)
-                                    !< Spatial step : do we need this?  (in)
-                                    !< Cell centerd Manning's n (intent out)
-                                    !< Cell center width (intent out)
-                                    !< any other argument we need here? 
-                                        
-     
+       subroutine spatial_data_sediment_if(ncell,     &
+                                           time,      &
+                                           dx,        &
+                                           dt,        &
+                                           manning,   &
+                                           width)
+       
+            use stm_precision
+            implicit none
+            
+            integer, intent(in) :: ncell                     !< Number of cells (in)
+            real(stm_real), intent(in)  :: time              !< Time of request (in) (do we need this for future extension?)
+            real(stm_real), intent(in)  :: dx                !< Spatial step : do we need this?  (in)
+            real(stm_real), intent(in)  :: dt                !< do we need this ? todo:
+            real(stm_real), intent(out) :: manning(ncell)    !< Cell centerd Manning's n (intent out)
+            real(stm_real), intent(out) :: width(ncell)      !< Cell center width (intent out)
+                                                             !< todo: any other argument we need here? 
   
      end subroutine
  
@@ -77,24 +85,46 @@ module suspended_sediment_variable
        !> Critical shear stress for surface erosion, Dry bulk density, Floc strength 
 
        !> This data might be provided by a subroutine
-       subroutine constant_data_sediment_if
+       subroutine constant_data_sediment_if(g_accelration,     &
+                                            kapa,              &
+                                            nu,                &
+                                            rho_w,             &
+                                            rho_s,             &
+                                            rho_floc,          &
+                                            diameter,          &
+                                            cohesive_diameter, &
+                                            ta_d_full,         &
+                                            rho_wet_bulk,      &
+                                            ta_d_particle,     &
+                                            ta_c_surf_erosion, &
+                                            rho_dry_bulk,      &
+                                            ta_floc,           &
+                                            time)
                                     
-                                 !< Gravitational acceleration(out)
-                                 !< von Karman's constant(out)
-                                 !< Kinematic viscosity of water(out)
-                                 !< Water density(out)
-                                 !< Sediment density(out)
-                                 !< Floc density(out)
-                                 !< Particle diameter(out)
-                                 !< Representative cohesive mixture diameter(out) 
-                                 !< Critical shear stress for full deposition (out)
-                                 !< Wet bulk density of the deposit (out)
-                                 !< Critical shear stress for partial deposition (out)
-                                 !< Critical shear stress for surface erosion (out)
-                                 !< Dry bulk density (out)
-                                 !< Floc strength (out)
+        use stm_precision
+        implicit none
+        
+        ! todo: NOTE: UNIT is important here
+        ! todo: Eli how we can set a whistleblower for wrong unit(s)[I mean SI and British] here?
+        ! todo: 
+        
+    real(stm_real), intent(out) ::  g_accelration       !< Gravitational acceleration(out)
+    real(stm_real), intent(out) ::  kapa                !< von Karman's constant(out)
+    real(stm_real), intent(out) ::  nu                  !< Kinematic viscosity of water(out)
+    real(stm_real), intent(out) ::  rho_w               !< Water density(out)
+    real(stm_real), intent(out) ::  rho_s               !< Sediment density(out)
+    real(stm_real), intent(out) ::  rho_floc            !< Floc density(out)
+    real(stm_real), intent(out) ::  diameter            !< Particle diameter(out)
+    real(stm_real), intent(out) ::  cohesive_diameter   !< Representative cohesive mixture diameter(out) 
+    real(stm_real), intent(out) ::  ta_d_full           !< Critical shear stress for full deposition (out)
+    real(stm_real), intent(out) ::  rho_wet_bulk        !< Wet bulk density of the deposit (out)
+    real(stm_real), intent(out) ::  ta_d_particle       !< Critical shear stress for partial deposition (out)
+    real(stm_real), intent(out) ::  ta_c_surf_erosion   !< Critical shear stress for surface erosion (out)
+    real(stm_real), intent(out) ::  rho_dry_bulk        !< Dry bulk density (out)
+    real(stm_real), intent(out) ::  ta_floc             !< Floc strength (out)
+    real(stm_real), intent(in)  ::  time                !< Time todo: do we need more parameters here? 
 
-                           
+                                
      end subroutine
  
  end interface
@@ -102,39 +132,119 @@ module suspended_sediment_variable
  !> Manning's n and channel width for the sediment source sink routine.
   procedure(constant_data_sediment_if),pointer :: fill_constant_sediment_data  => null()
  
- !> I can 
+ !> todo: fill here 
  contains
  
  !> Example spatial and time variables that prints an error and bails
- subroutine example_spatiotemporal_data_sediment
+ subroutine example_spatiotemporal_data_sediment(velocity,  &
+                                                 depth,     &
+                                                 ncell,     &
+                                                 time,      &
+                                                 dx,        &
+                                                 dt)
      use stm_precision
      use error_handling
      implicit none
      
-        call stm_fatal('ERROR HERE!')
+        integer, intent(in) :: ncell                    !< Number of cells (in)
+        real(stm_real), intent(in)  :: time             !< Time of request (in)
+        real(stm_real), intent(in)  :: dx               !< Spatial step (in)
+        real(stm_real), intent(in)  :: dt               !< Time step  (in)
+        real(stm_real), intent(out) :: velocity(ncell)  !< Cell and time centered velocity (out)
+        real(stm_real), intent(out) :: depth(ncell)     !< Cell center depth (out)
+        ! just to avoid warning
+        velocity = LARGEREAL
+        depth = minus* LARGEREAL
+     
+        call stm_fatal('ERROR IN SPATIOTEMPORAL DATA !')
  
  end subroutine 
  
   !> Example spatial variables that prints an error and bails
- subroutine example_spatial_data_sediment
+ subroutine example_spatial_data_sediment(ncell,     &
+                                          time,      &
+                                          dx,        &
+                                          dt,        &
+                                          manning,   &
+                                          width)
+ 
+ 
+ 
     use stm_precision
-     use error_handling
-     implicit none
+    use error_handling
+    implicit none
+               
+            integer, intent(in) :: ncell                     !< Number of cells (in)
+            real(stm_real), intent(in)  :: time              !< Time of request (in) (do we need this for future extension?)
+            real(stm_real), intent(in)  :: dx                !< Spatial step : do we need this?  (in)
+            real(stm_real), intent(in)  :: dt                !< do we need this ? todo:
+            real(stm_real), intent(out) :: manning(ncell)    !< Cell centerd Manning's n (intent out)
+            real(stm_real), intent(out) :: width(ncell)      !< Cell center width (intent out)
+                                                             !< todo: any other argument we need here? 
+       ! just to avoid warning
+        manning = minus*LARGEREAL
+        width = LARGEREAL
      
-        call stm_fatal('ERROR HERE!')
+        call stm_fatal('ERROR IN SPATIAL DATA !')
  
  
  end subroutine
  
  !> Example constants that prints an error and bails
- subroutine example_constant_data_sedimet
+ subroutine example_constant_data_sedimet(g_accelration,     &
+                                          kapa,              &
+                                          nu,                &
+                                          rho_w,             &
+                                          rho_s,             &
+                                          rho_floc,          &
+                                          diameter,          &
+                                          cohesive_diameter, &
+                                          ta_d_full,         &
+                                          rho_wet_bulk,      &
+                                          ta_d_particle,     &
+                                          ta_c_surf_erosion, &
+                                          rho_dry_bulk,      &
+                                          ta_floc,           &
+                                          time)
+ 
      use stm_precision
      use error_handling
      implicit none
-     
-        call stm_fatal('ERROR HERE!')
- 
-  
+    real(stm_real), intent(out) ::  g_accelration       !< Gravitational acceleration(out)
+    real(stm_real), intent(out) ::  kapa                !< von Karman's constant(out)
+    real(stm_real), intent(out) ::  nu                  !< Kinematic viscosity of water(out)
+    real(stm_real), intent(out) ::  rho_w               !< Water density(out)
+    real(stm_real), intent(out) ::  rho_s               !< Sediment density(out)
+    real(stm_real), intent(out) ::  rho_floc            !< Floc density(out)
+    real(stm_real), intent(out) ::  diameter            !< Particle diameter(out)
+    real(stm_real), intent(out) ::  cohesive_diameter   !< Representative cohesive mixture diameter(out) 
+    real(stm_real), intent(out) ::  ta_d_full           !< Critical shear stress for full deposition (out)
+    real(stm_real), intent(out) ::  rho_wet_bulk        !< Wet bulk density of the deposit (out)
+    real(stm_real), intent(out) ::  ta_d_particle       !< Critical shear stress for partial deposition (out)
+    real(stm_real), intent(out) ::  ta_c_surf_erosion   !< Critical shear stress for surface erosion (out)
+    real(stm_real), intent(out) ::  rho_dry_bulk        !< Dry bulk density (out)
+    real(stm_real), intent(out) ::  ta_floc             !< Floc strength (out)
+    real(stm_real), intent(in)  ::  time                !< Time todo: do we need more parameters here? 
+
+! just to avoid warning
+        g_accelration    = LARGEREAL  
+        kapa             = LARGEREAL  
+        nu               = LARGEREAL 
+        rho_w            = LARGEREAL 
+        rho_s            = LARGEREAL 
+        rho_floc         = LARGEREAL 
+        diameter         = LARGEREAL 
+        cohesive_diameter= LARGEREAL 
+        ta_d_full        = LARGEREAL 
+        rho_wet_bulk     = LARGEREAL 
+        ta_d_particle    = LARGEREAL 
+        ta_c_surf_erosion= LARGEREAL 
+        rho_dry_bulk     = LARGEREAL 
+        ta_floc          = LARGEREAL 
+                    
+   
+        call stm_fatal('ERROR IN CONSTANT PARAMETERS OF SEDIMENT!')
+   
  end subroutine  
   
 end module
