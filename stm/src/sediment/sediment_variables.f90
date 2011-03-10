eb6fdf7d2dbbea177b2a6524c9a4ec55bb081cae
kavehzamani <kavehzamani@93266118-e052-11de-8dcd-9b777bee35ae>
2011-03-10 18:17:45 +0000

    Rename some variables. Hereafter we remove deposition.f90, entrainment.f90, entrainment_10.f90 and sediment_source.f90 from svn

--- stm/src/sediment/sediment_variables.f90
+++ stm/src/sediment/sediment_variables.f90
@@ -32,19 +32,16 @@ module sediment_variables
  real(stm_real), save :: gravity                  !< Acceleration of gravity; it must be in SI units (constant)
  real(stm_real), save :: water_density            !< Water density
  real(stm_real), save :: sediment_density         !< Sediment density
- real(stm_real), save :: gravity                  !< Gravitational acceleration in SI unit
- real(stm_real), save :: water_density            !< Water density
- real(stm_real), save :: sediment_density         !< Sediment density
- real(stm_real), save :: kapa                     !< von Karman's constant
+ real(stm_real), save :: kappa                    !< von Karman's constant
  real(stm_real), save :: kinematic_viscosity      !< Kinematic viscosity of water    
  real(stm_real), save :: floc_density             !< Floc density
  real(stm_real), save :: diameter                 !< Particle diameter
  real(stm_real), save :: cohesive_diameter        !< Representative cohesive mixture diameter     
- real(stm_real), save :: ta_d_full                !< Critical shear stress for full deposition    
- real(stm_real), save :: rho_wet_bulk             !< Wet bulk density of the deposit
- real(stm_real), save :: ta_d_particle            !< Critical shear stress for partial deposition
- real(stm_real), save :: ta_c_surf_erosion        !< Critical shear stress for surface erosion 
- real(stm_real), save :: rho_dry_bulk             !< Dry bulk density 
+ real(stm_real), save :: crit_stress_full_dep     !< Critical shear stress for full deposition    
+ real(stm_real), save :: density_wet_bulk         !< Wet bulk density of the deposit
+ real(stm_real), save :: crit_stress_partial_dep  !< Critical shear stress for partial deposition
+ real(stm_real), save :: crit_stress_surf_erosion !< Critical shear stress for surface erosion 
+ real(stm_real), save :: density_dry_bulk         !< Dry bulk density 
  real(stm_real), save :: ta_floc                  !< Floc strength 
  !> Spatial sediment parameters
  real(stm_real), save, allocatable :: manning_n(:)!< Manning's n 
@@ -60,36 +57,36 @@ module sediment_variables
  real(stm_real) :: gravity                     !< Gravitational acceleration in SI unit
  real(stm_real) :: water_density               !< Water density
  real(stm_real) :: sediment_density            !< Sediment density
- real(stm_real) :: kapa                        !< von Karman's constant
+ real(stm_real) :: kappa                        !< von Karman's constant
  real(stm_real) :: kinematic_viscosity         !< Kinematic viscosity of water    
  real(stm_real) :: floc_density                !< Floc density
  real(stm_real) :: diameter                    !< Particle diameter
  real(stm_real) :: cohesive_diameter           !< Representative cohesive mixture diameter       
- real(stm_real) :: ta_d_full                   !< Critical shear stress for full deposition    
- real(stm_real) :: rho_wet_bulk                !< Wet bulk density of the deposit
- real(stm_real) :: ta_d_particle               !< Critical shear stress for partial deposition
- real(stm_real) :: ta_c_surf_erosion           !< Critical shear stress for surface erosion 
- real(stm_real) :: rho_dry_bulk                !< Dry bulk density 
+ real(stm_real) :: crit_stress_full_dep        !< Critical shear stress for full deposition    
+ real(stm_real) :: density_wet_bulk            !< Wet bulk density of the deposit
+ real(stm_real) :: crit_stress_partial_dep     !< Critical shear stress for partial deposition
+ real(stm_real) :: crit_stress_surf_erosion    !< Critical shear stress for surface erosion 
+ real(stm_real) :: density_dry_bulk            !< Dry bulk density 
  real(stm_real) :: ta_floc                     !< Floc strength 
   
  ! todo: QUESTION this is a derived variable based on constants so should it be here at all?
  !real(stm_real) :: specific_submerged_gravity 
  !specific_submerged_gravity = LARGEREAL
  
- gravity             = LARGEREAL
- water_density       = LARGEREAL
- sediment_density    = LARGEREAL
- kapa                = LARGEREAL
- kinematic_viscosity = LARGEREAL
- floc_density        = LARGEREAL
- diameter            = LARGEREAL
- cohesive_diameter   = LARGEREAL
- ta_d_full           = LARGEREAL
- rho_wet_bulk        = LARGEREAL
- ta_d_particle       = LARGEREAL
- ta_c_surf_erosion   = LARGEREAL
- rho_dry_bulk        = LARGEREAL
- ta_floc             = LARGEREAL             
+ gravity                   = LARGEREAL
+ water_density             = LARGEREAL
+ sediment_density          = LARGEREAL
+ kappa                     = LARGEREAL
+ kinematic_viscosity       = LARGEREAL
+ floc_density              = LARGEREAL
+ diameter                  = LARGEREAL
+ cohesive_diameter         = LARGEREAL
+ crit_stress_full_dep      = LARGEREAL
+ density_wet_bulk          = LARGEREAL
+ crit_stress_partial_dep   = LARGEREAL
+ crit_stress_surf_erosion  = LARGEREAL
+ density_dry_bulk          = LARGEREAL
+ ta_floc                   = LARGEREAL             
        
  return
  end subroutine
@@ -104,7 +101,7 @@ module sediment_variables
      allocate(width(ncell))
      
      manning_n = LARGEREAL
-     width = LARGEREAL
+     width     = LARGEREAL
      
      return
  end subroutine
@@ -122,7 +119,7 @@ module sediment_variables
  end subroutine
 
 
-!! todo: Considering structural point of view I think it has to move to suspended sediment varible 
+!! todo: We propose to move this to sediment_variables_derived.f90
 !!> Calculate shear stress
 !    subroutine calculate_shear_stress(shear_stress,    &
 !                                      velocity,        &
