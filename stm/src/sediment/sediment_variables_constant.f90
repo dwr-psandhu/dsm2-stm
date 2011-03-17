b86125adc30e57244bf2bb468eb24c20ea22c1b1
kavehzamani <kavehzamani@93266118-e052-11de-8dcd-9b777bee35ae>
2011-03-17 04:13:28 +0000

    diameter changed to d(nvar)

--- stm/src/sediment/sediment_variables_constant.f90
+++ stm/src/sediment/sediment_variables_constant.f90
@@ -26,8 +26,12 @@
 module sediment_variables
  use stm_precision
  
+ ! todo: I am not sure about the correct place of d(nvar) and 
+ ! todo: if we need d_cohesive(nvar) or not 
+ 
  integer :: ncell                                 !< Number of computational cells
- integer :: nvar                                  !< Number of variables
+ integer :: nvar                                  !< Number of variables non cohesive
+ !integer :: nvar_cohesive                        !< Number of variables cohesive
  !> Sediment constants   
  real(stm_real), save :: gravity                  !< Acceleration of gravity; it must be in SI units (constant)
  real(stm_real), save :: water_density            !< Water density
@@ -35,7 +39,7 @@ module sediment_variables
  real(stm_real), save :: kappa                    !< von Karman's constant
  real(stm_real), save :: kinematic_viscosity      !< Kinematic viscosity of water    
  real(stm_real), save :: floc_density             !< Floc density
- real(stm_real), save :: diameter                 !< Particle diameter
+ real(stm_real), save, allocatable :: diameter(:) !< Particle diameter
  real(stm_real), save :: cohesive_diameter        !< Representative cohesive mixture diameter     
  real(stm_real), save :: crit_stress_full_dep     !< Critical shear stress for full deposition    
  real(stm_real), save :: density_wet_bulk         !< Wet bulk density of the deposit
@@ -49,18 +53,17 @@ module sediment_variables
     
  contains
       
-!> Set sediment constants and calculated sediment constants
- subroutine set_sediment_constant()
+!> Set sediment constants 
+ subroutine set_sediment_constants
  use error_handling
  implicit none
- 
+    
  real(stm_real) :: gravity                     !< Gravitational acceleration in SI unit
  real(stm_real) :: water_density               !< Water density
  real(stm_real) :: sediment_density            !< Sediment density
- real(stm_real) :: kappa                        !< von Karman's constant
+ real(stm_real) :: kappa                       !< von Karman's constant
  real(stm_real) :: kinematic_viscosity         !< Kinematic viscosity of water    
  real(stm_real) :: floc_density                !< Floc density
- real(stm_real) :: diameter                    !< Particle diameter
  real(stm_real) :: cohesive_diameter           !< Representative cohesive mixture diameter       
  real(stm_real) :: crit_stress_full_dep        !< Critical shear stress for full deposition    
  real(stm_real) :: density_wet_bulk            !< Wet bulk density of the deposit
@@ -72,6 +75,7 @@ module sediment_variables
  ! todo: QUESTION this is a derived variable based on constants so should it be here at all?
  !real(stm_real) :: specific_submerged_gravity 
  !specific_submerged_gravity = LARGEREAL
+
  
  gravity                   = LARGEREAL
  water_density             = LARGEREAL
@@ -79,7 +83,6 @@ module sediment_variables
  kappa                     = LARGEREAL
  kinematic_viscosity       = LARGEREAL
  floc_density              = LARGEREAL
- diameter                  = LARGEREAL
  cohesive_diameter         = LARGEREAL
  crit_stress_full_dep      = LARGEREAL
  density_wet_bulk          = LARGEREAL
@@ -92,16 +95,19 @@ module sediment_variables
  end subroutine
     
   !> Allocate spatial sediment parameters 
- subroutine allocate_sediment_spatial_parameters(ncell)
+ subroutine allocate_sediment_parameters(ncell,nvar)
      use error_handling
      implicit none
-     integer,intent(in):: ncell    !<Number of cells
-                
+     integer,intent(in):: ncell    !< Number of cells
+     integer,intent(in):: nvar     !< Number of sediment classes
+            
      allocate(manning_n(ncell))
      allocate(width(ncell))
-     
+     allocate(diameter(nvar))
+         
      manning_n = LARGEREAL
      width     = LARGEREAL
+     diameter  = LARGEREAL
      
      return
  end subroutine
@@ -112,28 +118,14 @@ module sediment_variables
      implicit none
 
      ncell = 0
+     nvar  = 0 
    
      deallocate(manning_n)
+     deallocate(width)
+     deallocate(diameter)
    
      return
  end subroutine
-
-
-!! todo: We propose to move this to sediment_variables_derived.f90
-!!> Calculate shear stress
-!    subroutine calculate_shear_stress(shear_stress,    &
-!                                      velocity,        &
-!                                      ncell,           &
-!                                      density)
-!        implicit none
-!        real(stm_real), intent(out) :: shear_stress(ncell)        
-!        integer, intent(in)         :: ncell
-!        real(stm_real), intent(in)  :: velocity(ncell)
-!        real(stm_real), intent(in)  :: density
-!        
-!        return  
-!    end subroutine
-
 
 end module
 
