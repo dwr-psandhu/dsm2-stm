7a8e03ded08126dd377889ca79d78cc9448abbb1
kavehzamani <kavehzamani@93266118-e052-11de-8dcd-9b777bee35ae>
2011-04-07 23:56:17 +0000

    Some modification of test I1

--- trunk/stm/test/sediment/unit_test_non_cohesive_sediment.f90
+++ trunk/stm/test/sediment/unit_test_non_cohesive_sediment.f90
@@ -1,65 +1,120 @@
-!!<license>
-!!    Copyright (C) 1996, 1997, 1998, 2001, 2007, 2009 State of California,
-!!    Department of Water Resources.
-!!    This file is part of DSM2.
-!!
-!!    The Delta Simulation Model 2 (DSM2) is free software: 
-!!    you can redistribute it and/or modify
-!!    it under the terms of the GNU General Public License as published by
-!!    the Free Software Foundation, either version 3 of the License, or
-!!    (at your option) any later version.
-!!
-!!    DSM2 is distributed in the hope that it will be useful,
-!!    but WITHOUT ANY WARRANTY; without even the implied warranty of
-!!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-!!    GNU General Public License for more details.
-!!
-!!    You should have received a copy of the GNU General Public License
-!!    along with DSM2.  If not, see <http://www.gnu.org/licenses>.
-!!</license>
+!<license>
+!    Copyright (C) 1996, 1997, 1998, 2001, 2007, 2009 State of California,
+!    Department of Water Resources.
+!    This file is part of DSM2.
 !
-!!> Tests the suspended non cohesive sediment subroutines
-!!>@ingroup test sediment
-!module test_non_cohesive
+!    The Delta Simulation Model 2 (DSM2) is free software: 
+!    you can redistribute it and/or modify
+!    it under the terms of the GNU General Public License as published by
+!    the Free Software Foundation, either version 3 of the License, or
+!    (at your option) any later version.
 !
-!contains
+!    DSM2 is distributed in the hope that it will be useful,
+!    but WITHOUT ANY WARRANTY; without even the implied warranty of
+!    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!    GNU General Public License for more details.
 !
-!!> Tests Einsstein's first integral 
-!! todo: incase the main subroutine replaced somewhere else this counterpart should place in the correct test package
-!subroutine test_first_einstein_integral
-!
-!use fruit
-!use non_cohesive_source
-!use stm_precision
-!
-!implicit none
-!!---args
-!
-!real(stm_real)  :: exp_re_p(nclas)         !< Explicit particle reynolds number
-!real(stm_real)  :: diameter(nclas)         !< Particle diameter
-!real(stm_real)  :: capital_r               !< Submerged specific gravity of sediment particles  
-!!--- local
-!real(stm_real)  :: hand_calc_value
-!
-!diameter =  (/1d-2,2d-2,1d-3/)
-!g_acceleration = 9.81d0
-!capital_r = 1.65d0
-!kinematic_viscosity = 1.0d-6 
-!
-!!hand_calc_value = 
-! 
-!call explicit_particle_reynolds_number(exp_re_p,           &
-!                                       diameter,           &
-!                                       capital_r,          &
-!                                       g_acceleration,     &
-!                                       kinematic_viscosity,&
-!                                       nclas)
-!                                       
-!call assertEquals(hand_calc_value(1),exp_re_p(1),weak_eps,"Error in subroutine explicit_particle_reynolds_number!")
-!call assertEquals(hand_calc_value(2),exp_re_p(2),weak_eps,"Error in subroutine explicit_particle_reynolds_number!")
-!call assertEquals(hand_calc_value(3),exp_re_p(3),weak_eps,"Error in subroutine explicit_particle_reynolds_number!")
-!
-!return
-!end subroutine
-!
-!end module
+!    You should have received a copy of the GNU General Public License
+!    along with DSM2.  If not, see <http://www.gnu.org/licenses>.
+!</license>
+
+!> Tests the suspended non cohesive sediment subroutines
+!>@ingroup test sediment
+module test_non_cohesive
+
+contains
+
+!> Tests Einsstein's first integral 
+! todo: incase the main subroutine replaced somewhere else this counterpart should place in the correct test package
+subroutine test_first_einstein_integral
+
+use fruit
+use non_cohesive_source
+use stm_precision
+
+implicit none
+!---args
+
+real(stm_real)  :: rouse         !< Rouse dimenssionless number  
+real(stm_real)  :: delta         !< Relative bed layer thickness = b/H 
+real(stm_real)  :: J_1           !< First Einstein integral value     
+!--- local
+real(stm_real)  :: hand_calc_value
+
+delta = 0.01d0
+rouse = 0.1d0
+hand_calc_value = 0.630990839362793d0 !MATLAB calculation
+ 
+call first_einstein_integral(J_1,    &
+                             delta,  &
+                             rouse)
+                                       
+call assertEquals(hand_calc_value,J_1,weak_eps,"Error in subroutine first Einstein integral!")
+
+rouse = 0.7d0
+hand_calc_value = 0.075646372654714d0 !MATLAB calculation
+ 
+call first_einstein_integral(J_1,    &
+                             delta,  &
+                             rouse)
+                                       
+call assertEquals(hand_calc_value,J_1,weak_eps,"Error in subroutine first Einstein integral!")
+
+rouse = 1.7d0
+hand_calc_value = 0.011612330444738d0 !MATLAB calculation
+ 
+call first_einstein_integral(J_1,    &
+                             delta,  &
+                             rouse)
+                                       
+call assertEquals(hand_calc_value,J_1,weak_eps,"Error in subroutine first Einstein integral!")
+
+rouse = 2.7d0
+hand_calc_value = 0.005925241451994d0 !MATLAB calculation
+ 
+call first_einstein_integral(J_1,    &
+                             delta,  &
+                             rouse)
+                                       
+call assertEquals(hand_calc_value,J_1,weak_eps,"Error in subroutine first Einstein integral!")
+
+
+rouse = one
+hand_calc_value = 0.03651687056d0 !MATLAB calculation
+ 
+call first_einstein_integral(J_1,    &
+                             delta,  &
+                             rouse)
+                                       
+call assertEquals(hand_calc_value,J_1,weak_eps,"Error in subroutine first Einstein integral integer=1!")
+
+
+rouse = two
+hand_calc_value = 0.007954898765675d0 !MATLAB calculation
+ 
+call first_einstein_integral(J_1,    &
+                             delta,  &
+                             rouse)
+                                       
+call assertEquals(hand_calc_value,J_1,weak_eps,"Error in subroutine first Einstein integral integer=2!")
+
+rouse = three
+hand_calc_value =  0.004859662341771d0 !MATLAB calculation
+ 
+call first_einstein_integral(J_1,    &
+                             delta,  &
+                             rouse)
+                                       
+call assertEquals(hand_calc_value,J_1,weak_eps,"Error in subroutine first Einstein integral integer=3!")
+
+
+
+
+
+
+
+
+return
+end subroutine
+
+end module
