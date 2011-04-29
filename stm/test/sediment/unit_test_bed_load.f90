fca417925d0e02d45cbae0e1b340df0371618ee0
kavehzamani <kavehzamani@93266118-e052-11de-8dcd-9b777bee35ae>
2011-04-29 21:03:02 +0000

    bed load test is coded, still not working

--- stm/test/sediment/unit_test_bed_load.f90
+++ stm/test/sediment/unit_test_bed_load.f90
@@ -24,6 +24,54 @@ module test_bed_load
 
 contains 
 
+subroutine test_volumetric_bedload_transport_rate
+
+use fruit
+use bed_load
+use stm_precision
+
+implicit none
+integer,parameter:: nsed  = 2                     !< Number of sediment classes 
+integer,parameter:: ncell = 3                     !< Number of computational cell
+real(stm_real):: gravity = 9.8d0                  !< Gravity
+real(stm_real):: capital_r                        !< Submerged specific gravity of sediment particles  
+real(stm_real):: diameter(nsed)                   !< Particle diameter
+real(stm_real):: einstein_bedload_num(ncell,nsed) !< Dimensionless belload transport rate 
+real(stm_real):: q_sub_b(ncell,nsed)              !< Volumetric bedload transport rate
+!----------------
+real(stm_real):: hand_calculation(ncell,nsed)
+integer :: iclas 
+integer :: ivol
+
+capital_r = 1.65d0
+diameter = [0.1d-1, 0.2d-2]
+einstein_bedload_num(1,:) =[seven, three]
+einstein_bedload_num(2,:) =[ten, two]
+einstein_bedload_num(3,:) =[five , four]
+
+
+call volumetric_bedload_transport_rate(q_sub_b,                &
+                                       einstein_bedload_num,   &
+                                       diameter,               &
+                                       capital_r,              &
+                                       gravity,                &
+                                       nsed,                   &
+                                       ncell)
+                                       
+hand_calculation(1,:) =[0.028148357d0, 0.001079d0]
+hand_calculation(2,:) =[0.028148357d0, 0.001079d0]
+hand_calculation(3,:) =[0.028148357d0, 0.001079d0]
+
+do ivol=1,ncell
+    do iclas=1,nsed                                                                   
+        call assertEquals(hand_calculation(ivol,iclas),q_sub_b(ivol,iclas),weak_eps,"Error in volumetric_bedload_transport_rate"//char(ivol)//' & '//char(iclas))
+    end do
+end do
+
+
+return
+end subroutine
+
 
 
 end module  
